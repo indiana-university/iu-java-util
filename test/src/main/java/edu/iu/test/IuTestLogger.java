899655be5a8f75f6380ffca028c35803950de674
Mark Fyffe <mwfyffe@iu.edu>
2023-10-02 07:54:29 -0400

    STARCH-636 doc cleanup, platform loggers defined by property

--- test/src/main/java/edu/iu/test/IuTestLogger.java
+++ test/src/main/java/edu/iu/test/IuTestLogger.java
@@ -38,8 +38,12 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -51,10 +55,53 @@ import java.util.regex.Pattern;
 import org.opentest4j.AssertionFailedError;
 
 /**
- * Asserts that calls to {@link Logger#log(LogRecord)} unrelated to JUnit must
- * be expected, in order, by the test case being evaluated.
+ * Asserts that calls to {@link Logger#log(LogRecord)} must be expected by the
+ * test case being evaluated, and that all expected log events <em>must</em>
+ * occur in order.
+ * 
+ * <p>
+ * This mechanism is tied in automatically when depending on
+ * {@code iu-java-test} for unit testing. Any use of
+ * {@link Logger#log(LogRecord)} will cause the test to fail unless the log
+ * event is explicitly expected to be the next log event. Log event expectations
+ * <em>require</em>:
+ * </p>
+ * <ul>
+ * <li>Logger name to match exactly</li>
+ * <li>Log level to match exactly</li>
+ * <li>Log message to match a {@link Pattern regular expression}</li>
+ * <li>Thrown exception class to match exactly, or for an exception to not be
+ * thrown</li>
+ * <li>Thrown exception to meet additional criteria if defined by
+ * {@link Predicate}</li>
+ * </ul>
+ * 
+ * <p>
+ * <strong>Platform-level</strong> loggers are exempt, and will be logged
+ * normally as configured by {@link java.logging} before the test framework is
+ * configured, i.e., INFO level and higher logged to console. The following
+ * logger names are considered platform loggers.
+ * </p>
+ * <ul>
+ * <li>org.junit</li>
+ * <li>org.mockito</li>
+ * <li>java</li>
+ * <li>javax</li>
+ * <li>jakarta</li>
+ * <li>jdk</li>
+ * <li>com.sun</li>
+ * <li>com.oracle.</li>
+ * <li>oracle</li>
+ * <li>sun</li>
+ * <li>Any logger name prefixed by the comma-separated
+ * {@link IuTest#getProperty(String) test property value}
+ * {@code iu.util.test.platformLoggers}</li>
+ * </ul>
  */
 public final class IuTestLogger {
+
+	private static final Set<String> STANDARD_PLATFORM_LOGGER_NAMES = Set.of( //
+			"org.junit", "org.mockito", "java", "javax", "jakarta", "jdk", "com.sun", "com.oracle", "oracle", "sun");
 
 	private static class LogRecordMatcher<T extends Throwable> {
 		private final String loggerName;
@@ -141,20 +188,42 @@ public final class IuTestLogger {
 	private static IuTestLogHandler testHandler;
 	private static Level originalLevel;
 	private static Logger root;
+	private static Set<String> propertyDefinedPlatformLoggers;
 
 	static boolean isPlatformLogger(String loggerName) {
-		return loggerName.startsWith("org.junit.") || //
-				loggerName.startsWith("org.mockito.") || //
-				loggerName.startsWith("org.apache.") || //
-				loggerName.startsWith("java.") || //
-				loggerName.startsWith("javax.") || //
-				loggerName.startsWith("jakarta.") || //
-				loggerName.startsWith("jdk.") || //
-				loggerName.startsWith("com.sun.") || //
-				loggerName.startsWith("com.oracle.") || //
-				loggerName.startsWith("sun.");
+		if (STANDARD_PLATFORM_LOGGER_NAMES.contains(loggerName))
+			return true;
+
+		for (var standardPlatformLoggerName : STANDARD_PLATFORM_LOGGER_NAMES)
+			if (loggerName.startsWith(standardPlatformLoggerName + '.'))
+				return true;
+
+		if (propertyDefinedPlatformLoggers == null) {
+			var propertyDefinedPlatformLoggerNames = IuTest.getProperty("iu.util.test.platformLoggers");
+			if (propertyDefinedPlatformLoggerNames == null)
+				propertyDefinedPlatformLoggers = Collections.emptySet();
+			else {
+				Set<String> propertyDefinedPlatformLoggers = new HashSet<>();
+				for (var propertyDefinedPlatformLogger : List.of(propertyDefinedPlatformLoggerNames.split(","))) {
+					if (propertyDefinedPlatformLogger.charAt(propertyDefinedPlatformLogger.length() - 1) == '.')
+						propertyDefinedPlatformLogger = propertyDefinedPlatformLogger.substring(0,
+								propertyDefinedPlatformLogger.length() - 1);
+					propertyDefinedPlatformLoggers.add(propertyDefinedPlatformLogger);
+				}
+				IuTestLogger.propertyDefinedPlatformLoggers = propertyDefinedPlatformLoggers;
+			}
+		}
+
+		if (propertyDefinedPlatformLoggers.contains(loggerName))
+			return true;
+
+		for (var propertyDefinedPlatformLogger : propertyDefinedPlatformLoggers)
+			if (loggerName.startsWith(propertyDefinedPlatformLogger + '.'))
+				return true;
+
+		return false;
 	}
-	
+
 	static void init() {
 		root = LogManager.getLogManager().getLogger("");
 		originalLevel = root.getLevel();
@@ -178,6 +247,7 @@ public final class IuTestLogger {
 		assertTrue(testHandler.activeTest.equals(name));
 		testHandler.activeTest = null;
 		testHandler.assertExpectedMessages();
+		propertyDefinedPlatformLoggers = null;
 	}
 
 	static void destroy() {
