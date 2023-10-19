1d12fe31a71b3dd2edb8752e3830604ef56581f8
Mark Fyffe <mwfyffe@iu.edu>
2023-10-19 05:13:18 -0400

    STARCH-655 IuObject, IuIterable, decouple IuException from reflection API

---
+++ base/src/test/java/edu/iu/IuIterableTest.java
@@ -0,0 +1,244 @@
+/*
+ * Copyright © 2023 Indiana University
+ * All rights reserved.
+ *
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.iu;
+
+import static edu.iu.IuIterable.cat;
+import static edu.iu.IuIterable.empty;
+import static edu.iu.IuIterable.filter;
+import static edu.iu.IuIterable.iter;
+import static edu.iu.IuIterable.map;
+import static edu.iu.IuIterable.of;
+import static edu.iu.IuIterable.print;
+import static edu.iu.IuIterable.remaindersAreEqual;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("javadoc")
+public class IuIterableTest {
+
+	@Test
+	public void testNullToString() {
+		assertEquals("null", print((Iterable<?>) null));
+		assertEquals("null", print((Iterator<?>) null));
+	}
+
+	@Test
+	public void testEmptyToString() {
+		assertEquals("[]", print(() -> Collections.emptyIterator()));
+	}
+
+	@Test
+	public void testToStringOfOne() {
+		assertEquals("[one]", print(List.of("one")));
+	}
+
+	@Test
+	public void testToStringOfTwo() {
+		assertEquals("[one, two]", print(List.of("one", "two")));
+		assertEquals("[one, two]", print(IuIterable.iter("one", "two")));
+	}
+
+	@Test
+	public void testIteratorToStrings() {
+		var iter = iter("one", "two", "three").iterator();
+		assertEquals("[one, two, three]", iter.toString());
+		assertEquals("one", iter.next());
+		assertEquals("[..., two, three]", iter.toString());
+		assertEquals("two", iter.next());
+		assertEquals("[..., three]", iter.toString());
+		assertEquals("three", iter.next());
+		assertEquals("[...]", iter.toString());
+		assertFalse(iter.hasNext());
+	}
+
+	@Test
+	public void testHashCodeReturnsItemsRead() {
+		var iter = iter("one", "two", "three").iterator();
+		assertEquals(0, iter.hashCode());
+		assertEquals("one", iter.next());
+		assertEquals(1, iter.hashCode());
+		assertEquals("two", iter.next());
+		assertEquals(2, iter.hashCode());
+		assertEquals("three", iter.next());
+		assertEquals(3, iter.hashCode());
+		assertFalse(iter.hasNext());
+	}
+
+	@Test
+	public void testEqualsFollowsPosition() {
+		var iter1 = IuIterable.iter("one", "two", "three").iterator();
+		var iter2 = IuIterable.iter("two", "one", "three").iterator();
+		assertNotEquals(iter1, iter2);
+		assertNotEquals(iter1.next(), iter2.next());
+		assertNotEquals(iter1, iter2);
+		assertNotEquals(iter1.next(), iter2.next());
+		assertEquals(iter1, iter2);
+		assertEquals(iter1.next(), iter2.next());
+		assertEquals(iter1, iter2);
+		assertFalse(iter1.hasNext());
+		assertFalse(iter2.hasNext());
+	}
+
+	@Test
+	public void testNotEqualsNonFactory() {
+		var iter1 = iter("one", "two", "three").iterator();
+		var iter2 = List.of("two", "one", "three").iterator();
+		assertNotEquals(iter1, iter2);
+	}
+
+	@Test
+	public void testBasicFactoryToString() {
+		var source = List.of("two", "one", "three");
+		var iter = of(source::iterator);
+		assertEquals(source.toString(), iter.toString());
+		assertEquals(source.toString(), iter.iterator().toString());
+		assertEquals(source.toString(), print(iter));
+		assertEquals(source.toString(), print(iter.iterator()));
+	}
+
+	@Test
+	public void testEmpty() {
+		assertFalse(empty().iterator().hasNext());
+	}
+
+	@Test
+	public void testPrintSkipBounds() {
+		assertThrows(IllegalArgumentException.class, () -> print(null, -1));
+	}
+
+	@Test
+	public void testRemainderOfSameIsEquals() {
+		var iter1 = iter("one", "three").iterator();
+		assertTrue(remaindersAreEqual(iter1, iter1));
+	}
+
+	@Test
+	public void testMismatchByFirstItem() {
+		var i1 = iter("one");
+		var i2 = iter("two");
+		assertFalse(remaindersAreEqual(i1.iterator(), i2.iterator()));
+		assertFalse(remaindersAreEqual(i2.iterator(), i1.iterator()));
+	}
+
+	@Test
+	public void testMismatchByLaterItem() {
+		var i1 = iter("one");
+		var i2 = iter("one", "two");
+		assertFalse(remaindersAreEqual(i1.iterator(), i2.iterator()));
+		assertFalse(remaindersAreEqual(i2.iterator(), i1.iterator()));
+	}
+
+	@Test
+	public void testTailsMatch() {
+		var i1 = iter("one", "three");
+		var i2 = iter("two", "one", "three");
+		var iter2 = i2.iterator();
+		iter2.next();
+		assertTrue(remaindersAreEqual(i1.iterator(), iter2));
+	}
+
+	@Test
+	public void testNullAndEmptyArrays() {
+		assertSame(iter((Object[]) null), empty());
+		assertSame(iter(new Object[0]), empty());
+	}
+
+	@Test
+	public void testIterOutOfBounds() {
+		assertThrows(IndexOutOfBoundsException.class, () -> iter((Object[]) null, -1));
+		assertThrows(IndexOutOfBoundsException.class, () -> iter((Object[]) null, 1));
+		assertThrows(IndexOutOfBoundsException.class, () -> iter(new Object[] { "one" }, 2));
+	}
+
+	@Test
+	public void testNoSuchElementAfterArray() {
+		assertThrows(NoSuchElementException.class, () -> iter().iterator().next());
+		assertThrows(NoSuchElementException.class, () -> {
+			var iter = iter("one").iterator();
+			iter.next();
+			iter.next();
+		});
+	}
+
+	@Test
+	public void testCat() {
+		assertSame(empty(), cat());
+		var iter = iter();
+		assertSame(iter, cat(iter));
+		assertEquals("[one, two, three]", cat(iter("one"), iter("two", "three")).toString());
+	}
+
+	@Test
+	public void testNoSuchElementAfterCat() {
+		assertThrows(NoSuchElementException.class, () -> {
+			var iter = cat(iter("one"), iter("two", "three")).iterator();
+			iter.next();
+			iter.next();
+			iter.next();
+			iter.next();
+		});
+	}
+
+	@Test
+	public void testMap() {
+		var map = new LinkedHashMap<>();
+		map.put("one", 1);
+		map.put("two", 2);
+		assertEquals("[1, 2]", map(map.keySet(), map::get).toString());
+	}
+
+	@Test
+	public void testFilter() {
+		var set = Set.of("three", "one");
+		assertEquals("[one, three]", filter(iter("one", "two", "three"), set::contains).toString());
+	}
+
+	@Test
+	public void testNoSuchElementFromFilter() {
+		assertThrows(NoSuchElementException.class,
+				() -> filter(iter("one", "two", "three"), a -> false).iterator().next());
+	}
+
+}
---
+++ base/src/test/java/edu/iu/IuObjectTest.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright © 2023 Indiana University
+ * All rights reserved.
+ *
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.iu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("javadoc")
+public class IuObjectTest {
+
+	@Test
+	public void testCompareNullCheckForSameIsZero() {
+		assertEquals(0, IuObject.compareNullCheck("", ""));
+	}
+
+	@Test
+	public void testNullLessThanNonNull() {
+		assertEquals(-1, IuObject.compareNullCheck(null, ""));
+		assertEquals(1, IuObject.compareNullCheck("", null));
+	}
+
+	@Test
+	public void testMismatchNonNullsCheckToNull() {
+		assertNull(IuObject.compareNullCheck("a", "b"));
+	}
+
+	@Test
+	public void testCompareToMatchesNonNullNullCheck() {
+		assertEquals(IuObject.compareNullCheck("", ""), IuObject.compareTo("", ""));
+	}
+
+	@Test
+	public void testCompareUsesNaturalOrder() {
+		assertEquals(-1, IuObject.compareTo("a", "b"));
+		assertEquals(1, IuObject.compareTo("b", "a"));
+	}
+
+	@Test
+	public void testCompareUsesSystemOrderFoNonComparable() {
+		var a = new Object();
+		var b = new Object();
+		assertEquals(-IuObject.compareTo(a, b), IuObject.compareTo(b, a));
+	}
+
+	@Test
+	public void testCompareUsesSystemOrderForClassMismatch() {
+		var a = "";
+		var b = new Object();
+		assertEquals(-IuObject.compareTo(a, b), IuObject.compareTo(b, a));
+	}
+
+	@Test
+	public void testHashCode() {
+		assertEquals(31, IuObject.hashCode((Object) null));
+		assertEquals(40390, IuObject.hashCode(new boolean[] { true, false }));
+		assertEquals(1055, IuObject.hashCode(new byte[] { 2, 1 }));
+		assertEquals(4097, IuObject.hashCode(new char[] { 'a', 'b' }));
+		assertEquals(-2075230271, IuObject.hashCode(new double[] { 0.1, 2.3, 4.5 }));
+		assertEquals(-1142946842, IuObject.hashCode(new float[] { 0.1f, 2.3f }));
+		assertEquals(1089, IuObject.hashCode(new int[] { 3, 4 }));
+		assertEquals(1155, IuObject.hashCode(new long[] { 5, 8 }));
+		assertEquals(1232, IuObject.hashCode(new short[] { 8, -8 }));
+		assertEquals(-1100575154, IuObject.hashCode((Object) new String[] { "foo", "bar" }));
+		assertEquals(31, IuObject.hashCode(""));
+	}
+
+	@Test
+	public void testTypeCheckSameSame() {
+		assertTrue(IuObject.typeCheck(new ArrayList<>(), new ArrayList<>()));
+	}
+
+	@Test
+	public void testTypeCheckImplMismatch() {
+		assertFalse(IuObject.typeCheck(new ArrayList<>(), new HashSet<>()));
+	}
+
+	@Test
+	public void testTypeCheckCommonSuperClass() {
+		assertTrue(IuObject.typeCheck(new ArrayList<>(), new HashSet<>(), Collection.class));
+	}
+
+	@Test
+	public void testTypeCheckUnommonSuperClass() {
+		assertFalse(IuObject.typeCheck(new ArrayList<>(), new HashSet<>(), List.class));
+		assertFalse(IuObject.typeCheck(new HashSet<>(), new ArrayList<>(), List.class));
+	}
+
+	@Test
+	public void testTypeCheckNulls() {
+		assertTrue(IuObject.typeCheck(null, null));
+		assertFalse(IuObject.typeCheck(new Object(), null));
+		assertFalse(IuObject.typeCheck(null, new Object()));
+	}
+
+	@Test
+	public void testEqualsNulls() {
+		assertTrue(IuObject.equals(null, null));
+		assertFalse(IuObject.equals(new Object(), null));
+		assertFalse(IuObject.equals(null, new Object()));
+	}
+
+	@Test
+	public void testSetsAreEquals() {
+		var set1 = Set.of("a", "b", "c");
+		var set2 = new HashSet<>(set1);
+		assertTrue(IuObject.equals(set1, set2));
+		assertTrue(IuObject.equals(set2, set1));
+		set2.add("d");
+		assertFalse(IuObject.equals(set1, set2));
+		assertFalse(IuObject.equals(set2, set1));
+		set2.remove("c"); // same size again
+		assertFalse(IuObject.equals(set1, set2));
+		assertFalse(IuObject.equals(set2, set1));
+	}
+
+	@Test
+	public void testSortedSetsNotEquivalentToSet() {
+		var set1 = Set.of("foo", "bar", "baz");
+		var set2 = new TreeSet<>(set1);
+		assertEquals(IuObject.equals(set1, set2), IuObject.equals(set2, set1));
+	}
+
+	@Test
+	public void testIterablesAreEquals() {
+		var list = List.of("a", "b", "c");
+		var queue = new ArrayDeque<>(list);
+		assertTrue(IuObject.equals(list, queue));
+		assertTrue(IuObject.equals(queue, list));
+		queue.add("d");
+		assertFalse(IuObject.equals(list, queue));
+		assertFalse(IuObject.equals(queue, list));
+		queue.poll(); // same size again
+		assertFalse(IuObject.equals(list, queue));
+		assertFalse(IuObject.equals(queue, list));
+	}
+
+	@Test
+	public void testMapsAreEquals() {
+		var map1 = Map.of("foo", 1, "bar", 2, "baz", 3);
+		var map2 = new HashMap<>(map1);
+		assertTrue(IuObject.equals(map1, map2));
+		assertTrue(IuObject.equals(map2, map1));
+		map2.put("bar", 4);
+		assertFalse(IuObject.equals(map1, map2));
+		assertFalse(IuObject.equals(map2, map1));
+		map2.put("bar", 2);
+		map2.put("zzz", 4);
+		assertFalse(IuObject.equals(map1, map2));
+		assertFalse(IuObject.equals(map2, map1));
+	}
+
+	@Test
+	public void testDifferentClassesNotEquals() {
+		var map1 = Map.of("foo", 1, "bar", 2, "baz", 3);
+		var set1 = Set.of("foo", "bar", "baz");
+		assertFalse(IuObject.equals(set1, map1));
+		assertFalse(IuObject.equals(map1, set1));
+	}
+
+	@Test
+	public void testPrimitiveArrayEqualses() {
+		assertTrue(IuObject.equals(new boolean[] { true, false }, new boolean[] { true, false }));
+		assertTrue(IuObject.equals(new byte[] { 2, 1 }, new byte[] { 2, 1 }));
+		assertTrue(IuObject.equals(new char[] { 'a', 'b' }, new char[] { 'a', 'b' }));
+		assertTrue(IuObject.equals(new double[] { 0.1, 2.3, 4.5 }, new double[] { 0.1, 2.3, 4.5 }));
+		assertTrue(IuObject.equals(new float[] { 0.1f, 2.3f }, new float[] { 0.1f, 2.3f }));
+		assertTrue(IuObject.equals(new int[] { 3, 4 }, new int[] { 3, 4 }));
+		assertTrue(IuObject.equals(new long[] { 5, 8 }, new long[] { 5, 8 }));
+		assertTrue(IuObject.equals(new short[] { 8, -8 }, new short[] { 8, -8 }));
+	}
+
+	@Test
+	public void testObjectArrayEqualses() {
+		assertTrue(IuObject.equals(new String[] { "foo", "bar" }, new String[] { "foo", "bar" }));
+		assertFalse(IuObject.equals(new String[] { "foo", "bar", "baz" }, new String[] { "foo", "bar" }));
+		assertFalse(IuObject.equals(new String[] { "foo", "bar" }, new String[] { "foo", "bar", "baz" }));
+		assertFalse(IuObject.equals(new String[] { "foo", "baz" }, new String[] { "foo", "bar" }));
+		assertFalse(IuObject.equals(new String[] { "foo", "bar" }, new String[] { "foo", "baz" }));
+	}
+
+	@Test
+	public void testStringsAreEqual() {
+		assertTrue(IuObject.equals("", ""));
+	}
+
+	@Test
+	public void testObjectsAreNotEqual() {
+		assertFalse(IuObject.equals(new Object(), new Object()));
+	}
+}
