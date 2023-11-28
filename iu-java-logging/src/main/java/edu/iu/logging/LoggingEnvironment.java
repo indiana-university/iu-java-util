0ad739124ba828886459d02b3fc76a31870a0793
anderjak <anderjak@iu.edu>
2023-11-28 16:34:35 -0500

    STARCH-367. WIP add basic Javadoc

--- iu-java-logging/src/main/java/edu/iu/logging/LoggingEnvironment.java
+++ iu-java-logging/src/main/java/edu/iu/logging/LoggingEnvironment.java
@@ -1,4 +1,34 @@
-
+/*
+ * Copyright © 2023 Indiana University
+ * All rights reserved.
+ *
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package edu.iu.logging;
 
 import java.io.BufferedReader;
@@ -29,6 +59,9 @@ import java.util.logging.Logger;
 import edu.iu.logging.IuLoggingEnvironment.RuntimeMode;
 import iu.logging.IuLogHandler;
 
+/**
+ * Bootstrap logging environment
+ */
 public final class LoggingEnvironment {
 
 	private static final Map<String, String> PROPERTIES;
@@ -201,8 +234,8 @@ public final class LoggingEnvironment {
 		}
 	}
 
-	/*
-	 * IuLogHandler registered as root logger. Pre-initialize loggers to fine-tune
+	/**
+	 * Register IuLogHandler as root logger. Pre-initialize loggers to fine-tune
 	 * levels, once per class loader.
 	 */
 	public static void bootstrap(ClassLoader loader) {
