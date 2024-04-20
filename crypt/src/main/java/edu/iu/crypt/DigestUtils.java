4868539624a591f4723ab1438f1cfedb8d43276c
Mark Fyffe <mwfyffe@iu.edu>
2024-04-20 10:22:51 -0400

    STARCH-826 STARCH-817 implementations of JWS, JWE, and JWK

---
+++ crypt/src/main/java/edu/iu/crypt/DigestUtils.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2024 Indiana University
+ * All rights reserved.
+ *
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.iu.crypt;
+
+import java.security.MessageDigest;
+
+import edu.iu.IuException;
+
+/**
+ * Provides message digest and authentication utilities.
+ */
+public class DigestUtils {
+
+	private static final ThreadLocal<MessageDigest> SHA1 = new ThreadLocal<MessageDigest>() {
+		@Override
+		protected MessageDigest initialValue() {
+			return IuException.unchecked(() -> MessageDigest.getInstance("SHA-1"));
+		}
+	};
+
+	private static final ThreadLocal<MessageDigest> SHA256 = new ThreadLocal<MessageDigest>() {
+		@Override
+		protected MessageDigest initialValue() {
+			return IuException.unchecked(() -> MessageDigest.getInstance("SHA-256"));
+		}
+	};
+
+	private static final byte[] EMPTY_SHA1 = SHA1.get().digest(new byte[0]);
+	private static final byte[] EMPTY_SHA256 = SHA256.get().digest(new byte[0]);
+
+	/**
+	 * Gets a SHA-1 digest.
+	 * 
+	 * @param data character data
+	 * @return SHA-1 digest
+	 */
+	public static byte[] sha1(byte[] data) {
+		if (data == null || data.length == 0)
+			return EMPTY_SHA1;
+		return SHA1.get().digest(data);
+	}
+
+	/**
+	 * Gets a SHA-256 digest for character data.
+	 * <p>
+	 * The string passed into this method is first converted to UTF-8 binary format,
+	 * then digested.
+	 * </p>
+	 * 
+	 * @param data character data
+	 * @return SHA-256 digest
+	 */
+	public static byte[] sha256(byte[] data) {
+		if (data == null || data.length == 0)
+			return EMPTY_SHA256;
+		return SHA256.get().digest(data);
+	}
+
+	private DigestUtils() {
+	}
+}
---
+++ crypt/src/test/java/iu/crypt/JwsTest.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright © 2024 Indiana University
+ * All rights reserved.
+ *
+ * BSD 3-Clause License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package iu.crypt;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.jupiter.api.Test;
+
+import edu.iu.IdGenerator;
+import edu.iu.IuIterable;
+import edu.iu.IuText;
+import edu.iu.client.IuJson;
+import edu.iu.crypt.IuCryptTestCase;
+import edu.iu.crypt.WebCryptoHeader.Extension;
+import edu.iu.crypt.WebCryptoHeader.Param;
+import edu.iu.crypt.WebKey;
+import edu.iu.crypt.WebKey.Algorithm;
+import edu.iu.crypt.WebKey.Use;
+import edu.iu.crypt.WebSignature;
+import edu.iu.crypt.WebSignedPayload;
+import jakarta.json.JsonString;
+
+@SuppressWarnings("javadoc")
+public class JwsTest extends IuCryptTestCase {
+
+	@Test
+	public void testRFC8037_A_4() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+			NoSuchMethodException, SecurityException, ClassNotFoundException {
+		final var jwk = WebKey.parse("{\"kty\":\"OKP\",\"crv\":\"Ed25519\",\n"
+				+ "   \"d\":\"nWGxne_9WmC6hEr0kuwsxERJxWl7MmkZcDusAxyuf2A\",\n"
+				+ "   \"x\":\"11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo\"}");
+		assertEquals("{\"kty\":\"OKP\",\"crv\":\"Ed25519\",\"x\":\"11qYAYKxCrfVS_7TyWQHOg7hcvPapiMlrwIaaPcHURo\"}",
+				jwk.wellKnown().toString());
+
+		final var jws = WebSignature.builder(Algorithm.EDDSA).compact().key(jwk).sign("Example of Ed25519 signing");
+		final var compact = jws.compact();
+		assertEquals("eyJhbGciOiJFZERTQSJ9.RXhhbXBsZSBvZiBFZDI1NTE5IHNpZ25pbmc.hgyY0il_MGCj"
+				+ "P0JzlnLWG1PPOt7-09PGcvMg3AIbQR6dWbhijcNR4ki4iylGjg5BhVsPt9g7sVvpAr_MuM0KAg", compact);
+
+		final var fromCompact = WebSignedPayload.parse(compact);
+		assertEquals("{\"alg\":\"EdDSA\"}", fromCompact.getSignatures().iterator().next().getHeader().toString());
+		assertDoesNotThrow(() -> fromCompact.verify(jwk));
+
+		final var wrongKey = WebKey.ephemeral(Algorithm.EDDSA);
+		assertThrows(IllegalArgumentException.class, () -> fromCompact.verify(wrongKey));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testAllTheSignatures() {
+		final var extName = IdGenerator.generateId();
+		final var ext = mock(Extension.class, CALLS_REAL_METHODS);
+		when(ext.toJson(any())).thenAnswer(a -> IuJson.string((String) a.getArgument(0)));
+		when(ext.fromJson(any())).thenAnswer(a -> ((JsonString) a.getArgument(0)).getString());
+		Jose.register(extName, ext);
+
+		final Queue<Jwk> keys = new ArrayDeque<>();
+		final var algorithmIterator = IuIterable
+				.filter(IuIterable.iter(Algorithm.values()), a -> a.use.equals(Use.SIGN)).iterator();
+
+		var algorithm = algorithmIterator.next();
+		final var jwsBuilder = WebSignature.builder(algorithm);
+
+		var first = true;
+		do {
+			if (first)
+				first = false;
+			else
+				jwsBuilder.next(algorithm = algorithmIterator.next());
+			final var key = (Jwk) WebKey.ephemeral(algorithm);
+			keys.add(key);
+			jwsBuilder.key(key);
+			jwsBuilder.param(extName, IdGenerator.generateId());
+
+			final var id = IdGenerator.generateId();
+			final var compactJws = WebSignature.builder(algorithm).key(key).compact().sign(id);
+			final var fromCompact = WebSignedPayload.parse(compactJws.compact());
+			assertEquals(id, IuText.utf8(fromCompact.getPayload()));
+			fromCompact.verify(key);
+
+			final var fromSerial = WebSignedPayload.parse(compactJws.toString());
+			assertEquals(id, IuText.utf8(fromSerial.getPayload()));
+			fromSerial.verify(key);
+		} while (algorithmIterator.hasNext());
+
+		final var data = new byte[16384];
+		ThreadLocalRandom.current().nextBytes(data);
+		final var jws = jwsBuilder.sign(data);
+		assertThrows(IllegalStateException.class, () -> jws.compact());
+		final var serial = jws.toString();
+		final var fromSerial = WebSignedPayload.parse(serial);
+		keys.forEach(fromSerial::verify);
+		fromSerial.getSignatures().forEach(a -> verify(ext).verify(a));
+	}
+
+	@Test
+	public void testHeaderVerification() {
+		final var p = IuJson.object().add("alg", "HS256").build();
+		final var jose = new Jose(IuJson.object().add("alg", "HS384").build());
+		assertThrows(IllegalArgumentException.class, () -> new Jws(p, jose, null));
+
+		final var extName = ext();
+		final var p2 = IuJson.object().add("alg", "HS256").add(extName, IdGenerator.generateId()).build();
+		final var jose2 = new Jose(IuJson.object().add("alg", "HS256").add(extName, IdGenerator.generateId()).build());
+		assertThrows(IllegalArgumentException.class, () -> new Jws(p2, jose2, null));
+		assertDoesNotThrow(() -> new Jws(p2, new Jose(p2), null));
+	}
+
+	@Test
+	public void testInvalidCompact() {
+		final var key = WebKey.ephemeral(Algorithm.HS256);
+		final var jws = WebSignature.builder(Algorithm.HS256).key(key).compact().sign("foo");
+		assertThrows(IllegalArgumentException.class, () -> WebSignedPayload.parse(jws.compact() + ".blah"));
+	}
+
+	@Test
+	public void testInvalidAlgorithm() {
+		assertThrows(IllegalArgumentException.class, () -> WebSignature.builder(Algorithm.ECDH_ES));
+	}
+
+	@Test
+	public void testUnprotected() {
+		final var key = WebKey.ephemeral(Algorithm.HS256);
+		final var jws = WebSignature.builder(Algorithm.HS256).key(key).sign("foo");
+		assertNull(IuJson.parse(jws.toString()).asJsonObject().get("protected"));
+		assertThrows(NullPointerException.class, () -> WebSignedPayload.parse(jws.compact()));
+	}
+
+	@Test
+	public void testProtected() {
+		final var key = WebKey.ephemeral(Algorithm.HS256);
+		final var jws = WebSignature.builder(Algorithm.HS256).key(key).protect(Param.ALGORITHM).sign("foo");
+		assertNotNull(IuJson.parse(jws.toString()).asJsonObject().get("protected"));
+		assertDoesNotThrow(() -> WebSignedPayload.parse(jws.compact()));
+	}
+	
+	@Test
+	public void testTheWorks() {
+		final var id = IdGenerator.generateId();
+		final var key = WebKey.builder(Algorithm.EDDSA).keyId(id).ephemeral().build();
+		final var jwsBuilder = WebSignature.builder(Algorithm.EDDSA).wellKnown(key);
+		final var ext = ext();
+		jwsBuilder.protect(Param.ALGORITHM, Param.CONTENT_TYPE);
+		jwsBuilder.protect(ext);
+		jwsBuilder.crit(Param.ALGORITHM.name, ext);
+		jwsBuilder.keyId(id);
+		jwsBuilder.wellKnown(uri(WebKey.asJwks(IuIterable.iter(key.wellKnown()))));
+		jwsBuilder.param(Param.TYPE, "baz");
+		jwsBuilder.type("baz");
+		
+		jwsBuilder.contentType("foo").param(ext, "bar");
+		final var jws = jwsBuilder.sign("foo");
+		
+		assertDoesNotThrow(() -> WebSignedPayload.parse(jws.compact()).verify(key));
+		assertDoesNotThrow(() -> WebSignedPayload.parse(jws.toString()).verify(key));
+	}
+}
