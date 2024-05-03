0f7cecf99bc4633519f02fc45ef9211a7211fbbe
eshrastg <ess-dev-l@iu.edu>
2024-05-03 10:47:24 -0400

    clean feature/STARCH-828/type_loader_cleanup

--- docs/feature/STARCH-828/type_loader_cleanup/auth/basic/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"iu.auth.basic","l":"BasicAuthCredentials"},{"p":"iu.auth.basic","l":"BasicAuthSpi"}];updateSearchResults();
--- docs/feature/STARCH-828/type_loader_cleanup/auth/util/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"iu.auth.util","l":"AccessTokenVerifier"},{"p":"iu.auth.util","l":"AccessTokenVerifier.AlgorithmKey"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"iu.auth.util","l":"HttpUtils"},{"p":"iu.auth.util","l":"JsonProviderFactory"}];updateSearchResults();
