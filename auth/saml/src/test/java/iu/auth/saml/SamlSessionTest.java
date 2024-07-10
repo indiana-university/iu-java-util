a89b7cbca1ca5d6fcf0e3f43f6cb396f1d5e3b53
mramawat <mramawat@iu.edu>
2024-07-10 18:48:29 -0400

    unit test coverage, wip

--- auth/saml/src/test/java/iu/auth/saml/SamlSessionTest.java
+++ auth/saml/src/test/java/iu/auth/saml/SamlSessionTest.java
@@ -2,6 +2,7 @@ package iu.auth.saml;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
@@ -19,29 +20,44 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import edu.iu.IdGenerator;
 import edu.iu.IuException;
 import edu.iu.IuIterable;
 import edu.iu.auth.IuAuthenticationException;
+import edu.iu.auth.IuPrincipalIdentity;
 import edu.iu.auth.config.IuAuthenticationRealm;
 import edu.iu.auth.config.IuPrivateKeyPrincipal;
 import edu.iu.auth.config.IuSamlServiceProviderMetadata;
 import edu.iu.auth.saml.IuSamlSession;
 import edu.iu.crypt.WebEncryption.Encryption;
+import edu.iu.crypt.WebKey.Algorithm;
 import edu.iu.crypt.WebKey;
 import edu.iu.test.IuTestLogger;
 import iu.auth.config.AuthConfig;
 import iu.auth.config.IuSamlServiceProvider;
+import iu.auth.config.IuTrustedIssuer;
+
 
 @SuppressWarnings("javadoc")
 public class SamlSessionTest {
+	private MockedStatic<IuPrincipalIdentity> mockPrincipalIdentity;
+
 
 	@BeforeEach
-	public void setup() {
+	public void setup() throws Exception {
 		IuTestLogger.allow("iu.auth.saml.SamlSession", Level.FINE);
+		mockPrincipalIdentity = mockStatic(IuPrincipalIdentity.class);
+
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		mockPrincipalIdentity.close();
 	}
 
 	@Test
@@ -155,11 +171,13 @@ public class SamlSessionTest {
 
 	@Test
 	public void testVerifyResponse() throws MalformedURLException, IuAuthenticationException, NoSuchFieldException,
-			SecurityException, IllegalArgumentException, IllegalAccessException {
+	SecurityException, IllegalArgumentException, IllegalAccessException {
 		IuTestLogger.allow("iu.auth.saml.SamlSession", Level.INFO);
+		IuTestLogger.allow("iu.crypt.Jwe", Level.FINE);
 
 		File metaDataFile = new File("src/test/resource/metadata_sample.xml");
 		final var cert = mock(X509Certificate.class);
+
 		final var mockWebKey = mock(WebKey.class);
 		when(mockWebKey.getCertificateChain()).thenReturn(new X509Certificate[] { cert });
 		final var mockPkp = mock(IuPrivateKeyPrincipal.class);
@@ -175,12 +193,34 @@ public class SamlSessionTest {
 		final var realm = "iu-saml-test";
 
 		final var mockSamlPrincipal = mock(SamlPrincipal.class);
+		when(mockSamlPrincipal.toString()).thenReturn("{\n    \"iss\": \"https://sp.identityserver\",\n    "
+				+ "\"aud\": \"iu-saml-test\",\n    "
+				+ "\"sub\": \"test@iu.edu\",\n  "
+				+ "  \"iat\": 1720645746,\n   "
+				+ " \"exp\": 1720688946,\n    "
+				+ "\"auth_time\": 1720645746,\n   "
+				+ " \"urn:oasis:names:tc:SAML:2.0:assertion\": [\n "
+				+ "       {\n            "
+				+ "\"nbf\": 1720645746,\n           "
+				+ " \"exp\": 1720646046,\n           "
+				+ " \"authn_instant\": 1720645746,\n           "
+				+ " \"attribute_statement\": {\n                "
+				+ "\"urn:oid:2.16.840.1.113730.3.1.241\": \"ESS Testing\",\n                "
+				+ "\"displayName\": \"Testing\",\n               "
+				+ " \"urn:oid:0.9.2342.19200300.100.1.3\": \"test@iu.edu\",\n         "
+				+ "       \"mail\": \"test@iu.edu\",\n                "
+				+ "\"urn:oid:1.3.6.1.4.1.5923.1.1.1.6\": \"test@iu.edu\",\n                "
+				+ "\"eduPersonPrincipalName\": \"test@iu.edu\"\n            "
+				+ "}\n   }\n    ]\n}");
 
 		SamlBuilder builder = new SamlBuilder(config);
 		final var provider = mock(SamlServiceProvider.class);
 		when(provider.isValidEntryPoint(any())).thenReturn(true);
 		when(provider.getAuthnRequest(any(), any())).thenCallRealMethod();
 		when(provider.verifyResponse(any(), any(), any())).thenReturn(mockSamlPrincipal);
+		when(provider.getVerifyAlg()).thenReturn(WebKey.Algorithm.RS256);
+		when(provider.getVerifyKey()).thenReturn(WebKey.ephemeral(Algorithm.RSA_OAEP));		
+
 		Field f;
 		f = SamlServiceProvider.class.getDeclaredField("samlBuilder");
 		f.setAccessible(true);
@@ -193,8 +233,188 @@ public class SamlSessionTest {
 		try (final var mockIuAuthenticationRealm = mockStatic(IuAuthenticationRealm.class);
 				final var mockAuthConfig = mockStatic(AuthConfig.class, CALLS_REAL_METHODS);
 				final var mockProvider = mockStatic(SamlServiceProvider.class)) {
-
 			mockProvider.when(() -> SamlServiceProvider.withBinding(postUri)).thenReturn(provider);
+			mockPrincipalIdentity.when(() -> IuPrincipalIdentity.verify(any(), any())).thenReturn(true);
+
+			mockIuAuthenticationRealm.when(() -> IuAuthenticationRealm.of(realm)).thenReturn(config);
+			mockAuthConfig.when(() -> AuthConfig.get(IuSamlServiceProvider.class)).thenReturn(Arrays.asList(provider));
+			final var secret = WebKey.ephemeral(Encryption.A256GCM).getKey();
+
+			URI entryPointUri = URI.create("test://entrypoint");
+			IuSamlSession samlSession = new SamlSession(entryPointUri, postUri, () -> secret);
+			URI requestUri = samlSession.getRequestUri();
+			String parameters[] = requestUri.getQuery().split("&");
+			String relayState = parameters[1].split("=")[1];
+			assertDoesNotThrow(() -> samlSession.verifyResponse("127.0.0.0", "", relayState));
+			final var activatedSession = IuSamlSession.activate(samlSession.toString(), () -> secret);
+			assertNotNull(activatedSession);
+			final var iuSamlPrincipal = activatedSession.getPrincipalIdentity();
+			assertNotNull(iuSamlPrincipal);
+
+			activatedSession.getPrincipalIdentity();
+
+		}
+	}
+
+	@Test
+	public void testToString() throws MalformedURLException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		IuTestLogger.allow("iu.auth.saml.SamlSession", Level.INFO);
+		IuTestLogger.allow("iu.crypt.Jwe", Level.FINE);
+
+		File metaDataFile = new File("src/test/resource/metadata_sample.xml");
+		final var cert = mock(X509Certificate.class);
+
+		final var mockWebKey = mock(WebKey.class);
+		when(mockWebKey.getCertificateChain()).thenReturn(new X509Certificate[] { cert });
+		final var mockPkp = mock(IuPrivateKeyPrincipal.class);
+		when(mockPkp.getEncryptJwk()).thenReturn(mockWebKey);
+		when(mockPkp.getAlg()).thenReturn(WebKey.Algorithm.RS256);
+		when(mockPkp.getJwk()).thenReturn(mockWebKey);
+		final var uri = mock(URI.class);
+		when(uri.toURL()).thenReturn(metaDataFile.toPath().toUri().toURL());
+		final var acsUri = URI.create("test://postUrl/");
+		IuSamlServiceProviderMetadata config = getConfig(Arrays.asList(uri), "urn:iu:ess:sisjee", mockPkp,
+				Arrays.asList(acsUri));
+		final var postUri = URI.create("test://postUrl/");
+		final var realm = "iu-saml-test";
+
+		final var mockSamlPrincipal = mock(SamlPrincipal.class);
+		when(mockSamlPrincipal.toString()).thenReturn("{\n    \"iss\": \"https://sp.identityserver\",\n    "
+				+ "\"aud\": \"iu-saml-test\",\n    "
+				+ "\"sub\": \"test@iu.edu\",\n  "
+				+ "  \"iat\": 1720645746,\n   "
+				+ " \"exp\": 1720688946,\n    "
+				+ "\"auth_time\": 1720645746,\n   "
+				+ " \"urn:oasis:names:tc:SAML:2.0:assertion\": [\n "
+				+ "       {\n            "
+				+ "\"nbf\": 1720645746,\n           "
+				+ " \"exp\": 1720646046,\n           "
+				+ " \"authn_instant\": 1720645746,\n           "
+				+ " \"attribute_statement\": {\n                "
+				+ "\"urn:oid:2.16.840.1.113730.3.1.241\": \"ESS Testing\",\n                "
+				+ "\"displayName\": \"Testing\",\n               "
+				+ " \"urn:oid:0.9.2342.19200300.100.1.3\": \"test@iu.edu\",\n         "
+				+ "       \"mail\": \"test@iu.edu\",\n                "
+				+ "\"urn:oid:1.3.6.1.4.1.5923.1.1.1.6\": \"test@iu.edu\",\n                "
+				+ "\"eduPersonPrincipalName\": \"test@iu.edu\"\n            "
+				+ "}\n   }\n    ]\n}");
+
+		final var provider = mock(SamlServiceProvider.class);
+		when(provider.isValidEntryPoint(any())).thenReturn(true);
+		when(provider.getAuthnRequest(any(), any())).thenCallRealMethod();
+		when(provider.verifyResponse(any(), any(), any())).thenReturn(mockSamlPrincipal);
+		when(provider.getVerifyAlg()).thenReturn(WebKey.Algorithm.RS256);
+		when(provider.getVerifyKey()).thenReturn(WebKey.ephemeral(Algorithm.RSA_OAEP));		
+		try (final var mockIuAuthenticationRealm = mockStatic(IuAuthenticationRealm.class);
+				final var mockAuthConfig = mockStatic(AuthConfig.class, CALLS_REAL_METHODS);
+				final var mockProvider = mockStatic(SamlServiceProvider.class)) {
+			mockProvider.when(() -> SamlServiceProvider.withBinding(postUri)).thenReturn(provider);
+	
+			mockIuAuthenticationRealm.when(() -> IuAuthenticationRealm.of(realm)).thenReturn(config);
+			mockAuthConfig.when(() -> AuthConfig.get(IuSamlServiceProvider.class)).thenReturn(Arrays.asList(provider));
+			final var secret = WebKey.ephemeral(Encryption.A128GCM).getKey();
+
+			URI entryPointUri = URI.create("test://entrypoint");
+			IuSamlSession samlSession = new SamlSession(entryPointUri, postUri, () -> secret);
+			Field f;
+			f = SamlSession.class.getDeclaredField("relayState");
+			f.setAccessible(true);
+			f.set(samlSession, IdGenerator.generateId());
+
+			f = SamlSession.class.getDeclaredField("sessionId");
+			f.setAccessible(true);
+			f.set(samlSession, IdGenerator.generateId());
+			assertNotNull(samlSession.toString());
+			
+			samlSession = new SamlSession(entryPointUri, postUri, () -> WebKey.ephemeral(Encryption.A192GCM).getKey());
+			f = SamlSession.class.getDeclaredField("relayState");
+			f.setAccessible(true);
+			f.set(samlSession, IdGenerator.generateId());
+
+			f = SamlSession.class.getDeclaredField("sessionId");
+			f.setAccessible(true);
+			f.set(samlSession, IdGenerator.generateId());
+			assertNotNull(samlSession.toString());
+			
+			final var session = new SamlSession(entryPointUri, postUri, () -> new byte[8]);
+			f = SamlSession.class.getDeclaredField("relayState");
+			f.setAccessible(true);
+			f.set(session, IdGenerator.generateId());
+
+			f = SamlSession.class.getDeclaredField("sessionId");
+			f.setAccessible(true);
+			f.set(session, IdGenerator.generateId());
+			assertThrows(IllegalStateException.class, () -> session.toString());
+			
+		}
+	}
+
+
+	@Test
+	public void testGetPrincipalIdentityIuAuthenticationException() throws NoSuchFieldException, SecurityException, MalformedURLException, IllegalArgumentException, IllegalAccessException, IuAuthenticationException {
+		IuTestLogger.allow("iu.auth.saml.SamlSession", Level.INFO);
+		IuTestLogger.allow("iu.crypt.Jwe", Level.FINE);
+
+		File metaDataFile = new File("src/test/resource/metadata_sample.xml");
+		final var cert = mock(X509Certificate.class);
+
+		final var mockWebKey = mock(WebKey.class);
+		when(mockWebKey.getCertificateChain()).thenReturn(new X509Certificate[] { cert });
+		final var mockPkp = mock(IuPrivateKeyPrincipal.class);
+		when(mockPkp.getEncryptJwk()).thenReturn(mockWebKey);
+		when(mockPkp.getAlg()).thenReturn(WebKey.Algorithm.RS256);
+		when(mockPkp.getJwk()).thenReturn(mockWebKey);
+		final var uri = mock(URI.class);
+		when(uri.toURL()).thenReturn(metaDataFile.toPath().toUri().toURL());
+		final var acsUri = URI.create("test://postUrl/");
+		IuSamlServiceProviderMetadata config = getConfig(Arrays.asList(uri), "urn:iu:ess:sisjee", mockPkp,
+				Arrays.asList(acsUri));
+		final var postUri = URI.create("test://postUrl/");
+		final var realm = "iu-saml-test";
+
+		final var mockSamlPrincipal = mock(SamlPrincipal.class);
+		when(mockSamlPrincipal.toString()).thenReturn("{\n    \"iss\": \"https://sp.identityserver\",\n    "
+				+ "\"aud\": \"iu-saml-test\",\n    "
+				+ "\"sub\": \"test@iu.edu\",\n  "
+				+ "  \"iat\": 1720645746,\n   "
+				+ " \"exp\": 1720688946,\n    "
+				+ "\"auth_time\": 1720645746,\n   "
+				+ " \"urn:oasis:names:tc:SAML:2.0:assertion\": [\n "
+				+ "       {\n            "
+				+ "\"nbf\": 1720645746,\n           "
+				+ " \"exp\": 1720646046,\n           "
+				+ " \"authn_instant\": 1720645746,\n           "
+				+ " \"attribute_statement\": {\n                "
+				+ "\"urn:oid:2.16.840.1.113730.3.1.241\": \"ESS Testing\",\n                "
+				+ "\"displayName\": \"Testing\",\n               "
+				+ " \"urn:oid:0.9.2342.19200300.100.1.3\": \"test@iu.edu\",\n         "
+				+ "       \"mail\": \"test@iu.edu\",\n                "
+				+ "\"urn:oid:1.3.6.1.4.1.5923.1.1.1.6\": \"test@iu.edu\",\n                "
+				+ "\"eduPersonPrincipalName\": \"test@iu.edu\"\n            "
+				+ "}\n   }\n    ]\n}");
+
+		SamlBuilder builder = new SamlBuilder(config);
+		final var provider = mock(SamlServiceProvider.class);
+		when(provider.isValidEntryPoint(any())).thenReturn(true);
+		when(provider.getAuthnRequest(any(), any())).thenCallRealMethod();
+		when(provider.verifyResponse(any(), any(), any())).thenReturn(mockSamlPrincipal);
+		when(provider.getVerifyAlg()).thenReturn(WebKey.Algorithm.RS256);
+		when(provider.getVerifyKey()).thenReturn(WebKey.ephemeral(Algorithm.RSA_OAEP));		
+
+		Field f;
+		f = SamlServiceProvider.class.getDeclaredField("samlBuilder");
+		f.setAccessible(true);
+		f.set(provider, builder);
+
+		f = SamlServiceProvider.class.getDeclaredField("postUri");
+		f.setAccessible(true);
+		f.set(provider, postUri);
+
+		try (final var mockIuAuthenticationRealm = mockStatic(IuAuthenticationRealm.class);
+				final var mockAuthConfig = mockStatic(AuthConfig.class, CALLS_REAL_METHODS);
+				final var mockProvider = mockStatic(SamlServiceProvider.class)) {
+			mockProvider.when(() -> SamlServiceProvider.withBinding(postUri)).thenReturn(provider);
+			mockPrincipalIdentity.when(() -> IuPrincipalIdentity.verify(any(), any())).thenThrow(IuAuthenticationException.class);
 
 			mockIuAuthenticationRealm.when(() -> IuAuthenticationRealm.of(realm)).thenReturn(config);
 			mockAuthConfig.when(() -> AuthConfig.get(IuSamlServiceProvider.class)).thenReturn(Arrays.asList(provider));
@@ -206,11 +426,10 @@ public class SamlSessionTest {
 
 			String parameters[] = requestUri.getQuery().split("&");
 			String relayState = parameters[1].split("=")[1];
-			System.out.println("relay::: " + relayState);
 			assertDoesNotThrow(() -> samlSession.verifyResponse("127.0.0.0", "", relayState));
-			// final var activatedSession = IuSamlSession.activate(samlSession.toString(),
-			// () -> secret);
-
+			final var activatedSession = IuSamlSession.activate(samlSession.toString(), () -> secret);
+			assertNotNull(activatedSession);
+			assertThrows(IuAuthenticationException.class,() -> activatedSession.getPrincipalIdentity());
 		}
 	}
 
