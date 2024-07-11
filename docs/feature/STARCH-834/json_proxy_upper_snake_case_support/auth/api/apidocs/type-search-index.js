69946ac938e6e7cbe1548c81014c000807f18ec0
eshrastg <ess-dev-l@iu.edu>
2024-07-11 10:23:33 -0400

    publish james-anderson-iu feature/STARCH-834/json_proxy_upper_snake_case_support

---
+++ docs/feature/STARCH-834/json_proxy_upper_snake_case_support/auth/api/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.iu.auth.spi","l":"IuAuthConfigSpi"},{"p":"edu.iu.auth","l":"IuAuthenticationException"},{"p":"edu.iu.auth","l":"IuPrincipalIdentity"},{"p":"edu.iu.auth.spi","l":"IuPrincipalSpi"}];updateSearchResults();
