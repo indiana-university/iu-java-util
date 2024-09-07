2ae3c43c2fbad41cdd04504f0c3508ca988e33c3
eshrastg <ess-dev-l@iu.edu>
2024-09-07 08:00:17 -0400

    clean feature/STARCH-884/simplify_idgenerator

--- docs/feature/STARCH-884/simplify_idgenerator/type/bundle/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.iu.type.bundle","l":"IuTypeBundle"},{"p":"iu.type.bundle","l":"TypeBundleSpi"}];updateSearchResults();
