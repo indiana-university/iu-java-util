dd8fd6468d63a71bdf47b9e60f6e6f46875befbb
eshrastg <ess-dev-l@iu.edu>
2024-12-19 08:00:56 -0500

    publish mwfyffeiu feature/STARCH-933/component_container

---
+++ docs/feature/STARCH-933/component_container/logging/api/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.iu.logging","l":"IuLogContext"},{"p":"edu.iu.logging","l":"IuLogEvent"}];updateSearchResults();
