324f04b9ef83de28d25449a5b1f58829a0c343c9
anderjak <anderjak@iu.edu>
2025-01-10 19:24:51 +0000

    initial port from ess-build. start on coverage

---
+++ el/src/main/java/edu/iu/util/el/ElContext.java
@@ -0,0 +1,402 @@
+package edu.iu.util.el;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import edu.iu.IuStream;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonNumber;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+
+/**
+ * 
+ */
+public class ElContext {
+
+	private static final Logger LOG = Logger.getLogger(ElContext.class.getName());
+
+	private static char START_TOKEN = '{';
+	private static char TEMPLATE_TOKEN = '<';
+	private static char INLINE_TOKEN = '`';
+	private static char END_TOKEN = '}';
+	private static String START = Character.toString(START_TOKEN);
+
+	private static class TemplateExpression {
+		private final int insertPoint;
+		private final String expression;
+
+		private TemplateExpression(int insertPoint, String expression) {
+			this.insertPoint = insertPoint;
+			this.expression = expression;
+		}
+	}
+
+	private static class Template {
+		private final String content;
+		private final List<TemplateExpression> expressions;
+
+		private Template(String content) {
+			if (content == null) {
+				this.content = "";
+				this.expressions = Collections.emptyList();
+				return;
+			}
+
+			int iot = content.indexOf(START_TOKEN);
+			if (iot == -1) {
+				this.content = content;
+				this.expressions = Collections.emptyList();
+				return;
+			}
+
+			List<TemplateExpression> el = new ArrayList<>();
+			StringBuilder contentBuffer = new StringBuilder(content);
+			while (iot != -1) {
+
+				if (iot > 0 && contentBuffer.charAt(iot - 1) == El.ESC_TOKEN) {
+					contentBuffer.deleteCharAt(iot - 1);
+					iot = contentBuffer.indexOf(START, iot);
+					continue;
+				}
+
+				if (iot > 0 && iot < contentBuffer.length() //
+						&& contentBuffer.charAt(iot - 1) == '$' //
+						&& contentBuffer.charAt(iot) == '{') {
+					iot = contentBuffer.indexOf(START, iot + 1);
+					continue;
+				}
+
+				int length = contentBuffer.length();
+				int depth = 0;
+				int sdepth = 1;
+				int ioe;
+				for (ioe = iot + 1; ioe < length; ioe++) {
+					char c = contentBuffer.charAt(ioe);
+					if (c == INLINE_TOKEN) {
+						char p = contentBuffer.charAt(ioe - 1);
+						if (p == TEMPLATE_TOKEN)
+							depth++;
+						else if (depth > 0 && ioe + 1 < length && contentBuffer.charAt(ioe + 1) == END_TOKEN)
+							depth--;
+					} else if (c == START_TOKEN)
+						sdepth++;
+					else if (c == END_TOKEN) {
+						sdepth--;
+						if (depth == 0 && sdepth == 0)
+							break;
+					}
+				}
+				if (ioe == length)
+					throw new IllegalStateException("Missing end token '`}': " + content.substring(iot));
+
+				el.add(new TemplateExpression(iot, contentBuffer.substring(iot + 1, ioe)));
+				contentBuffer.delete(iot, ioe + 1);
+
+				iot = contentBuffer.indexOf(START, iot);
+			}
+			this.content = contentBuffer.toString();
+			this.expressions = el;
+		}
+
+		private void apply(boolean first, JsonValue key, JsonValue value, ElContext context,
+				Deque<ElContext> evalStack) {
+			int offset = context.templateBuffer.length();
+			context.templateBuffer.append(content);
+			for (TemplateExpression expr : expressions) {
+				ElContext elc = new ElContext(context, first, key, value.asJsonObject(), expr.expression);
+				elc.insertPoint = offset + expr.insertPoint;
+
+				// push template expressions in order found, to process the last
+				// expression first and avoid the need to adjust insert points
+				evalStack.push(elc);
+			}
+		}
+	}
+
+	private final JsonValue root;
+	private final ElContext parent;
+	private final boolean head;
+	private final JsonValue index;
+	private final JsonObject context;
+
+	private final String expression;
+	private final Map<String, Template> templateCache;
+
+	private boolean raw;
+	private int position;
+	private JsonValue result;
+	private JsonValue matchResult;
+	private JsonValue lastResult;
+
+	private boolean template;
+	private String templatePath;
+	private StringBuilder templateBuffer;
+	private int insertPoint = -1;
+
+	ElContext(ElContext parent, boolean head, JsonValue index, JsonObject context, String expression) {
+		this.parent = parent;
+		this.head = head;
+		this.index = index;
+		this.context = context;
+		this.expression = expression;
+
+		if (parent == null) {
+			this.root = context;
+			this.templateCache = new HashMap<>();
+		} else {
+			this.root = parent.root;
+			this.templateCache = parent.templateCache;
+		}
+
+		setResult(context);
+	}
+
+	ElContext(ElContext replace, String expression) {
+		this.parent = replace.parent;
+		this.head = replace.head;
+		this.index = replace.index;
+		this.context = replace.context;
+		this.root = replace.root;
+		this.templateCache = replace.templateCache;
+		this.insertPoint = replace.insertPoint;
+
+		this.expression = expression;
+		this.lastResult = replace.getResult();
+		setResult(context);
+	}
+
+	boolean isEmpty() {
+		return expression == null || position == -1 || position >= expression.length();
+	}
+
+	int getPosition() {
+		return position;
+	}
+
+	void advancePosition(int position) {
+		this.position += position;
+	}
+
+	void setPositionAtEnd() {
+		this.position = expression.length();
+	}
+
+	boolean isRaw() {
+		return raw;
+	}
+
+	void markAsRaw() {
+		this.raw = true;
+	}
+
+	String getExpression() {
+		return isEmpty() ? "" : expression.substring(position);
+	}
+
+	JsonValue getResult() {
+		if (matchResult == null)
+			return result;
+
+		if (result == null)
+			return JsonValue.FALSE;
+
+		return matchResult.equals(result) ? JsonValue.TRUE : JsonValue.FALSE;
+	}
+
+	void setResult(JsonValue result) {
+		this.result = result;
+	}
+
+	void setMatchResult(JsonValue matchResult) {
+		this.matchResult = matchResult;
+	}
+
+	void postProcessResult(Deque<ElContext> evalStack) {
+		if (template)
+			result = Json.createValue(templateBuffer.toString());
+
+		if (raw && parent == null)
+			return;
+
+		String resultText;
+		if (result == null || result.equals(JsonValue.NULL))
+			return;
+		else if (result instanceof JsonString)
+			resultText = ((JsonString) result).getString();
+		else if (result.equals(JsonValue.TRUE) || result.equals(JsonValue.FALSE) || (result instanceof JsonNumber))
+			resultText = result.toString();
+		else
+			throw new IllegalStateException("Non-atmoic result");
+
+		if (!raw)
+			result = Json.createValue(resultText = StringEscapeUtils.escapeHtml4(resultText));
+
+		if (parent != null && parent.template)
+			if (insertPoint == -1) // template name expression
+				parent.setupTemplate(resultText, evalStack);
+			else
+				parent.templateBuffer.insert(insertPoint, resultText);
+	}
+
+	boolean isTemplate() {
+		return template;
+	}
+
+	void markAsTemplate() {
+		this.template = true;
+		this.raw = true;
+	}
+
+	private void setupTemplate(String path, Deque<ElContext> evalStack) {
+		assert template && templatePath == null && templateBuffer == null;
+
+		String resourcePath = path == null ? "" : path;
+		boolean inline = resourcePath.length() > 1 && resourcePath.charAt(0) == '`'
+				&& resourcePath.charAt(resourcePath.length() - 1) == '`';
+		if (!inline) {
+			int ioc = resourcePath.indexOf(':');
+			if (ioc == -1 && (resourcePath.isEmpty() || resourcePath.charAt(0) != '/')) {
+				String parentDir;
+				String parentPath = parent == null ? null : parent.templatePath;
+				if (parentPath == null)
+					parentDir = "";
+				else {
+					int lioc = parentPath.indexOf(':') + 1;
+					int lios = parentPath.lastIndexOf('/');
+					parentDir = parentPath.substring(0, lios < lioc ? lioc : lios);
+				}
+
+				if (resourcePath.isEmpty())
+					resourcePath = parentDir;
+				else if (parentDir != null && !parentDir.isEmpty())
+					resourcePath = (parentDir + '/' + resourcePath).intern();
+			}
+
+			// Strip leading slash from resource path
+			if (resourcePath.isEmpty() || (resourcePath.length() == 1 && resourcePath.charAt(0) == '/'))
+				resourcePath = "";
+			else if (resourcePath.charAt(0) == '/')
+				resourcePath = resourcePath.substring(1);
+		}
+
+		templatePath = resourcePath.intern();
+
+		Template template = templateCache.get(templatePath);
+		if (template == null)
+			if (inline)
+				template = new Template(resourcePath.substring(1, resourcePath.length() - 1));
+			else {
+				URL rurl = Thread.currentThread().getContextClassLoader().getResource(resourcePath);
+				if (rurl == null) {
+					LOG.fine(() -> "Resource " + templatePath + " (" + path + ") not found");
+					throw new IllegalArgumentException(resourcePath);
+				}
+
+				try (InputStream is = rurl.openStream(); Reader r = new InputStreamReader(is)) {
+					templateCache.put(templatePath, template = new Template(IuStream.read(r)));
+				} catch (IOException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+
+		templateBuffer = new StringBuilder();
+
+		if (result instanceof JsonArray) {
+			var array = result.asJsonArray();
+			for (int i = 0; i < array.size(); i++)
+				template.apply(i == 0, Json.createValue(i), array.get(0), this, evalStack);
+		} else
+			template.apply(false, null, result, this, evalStack);
+	}
+
+	/**
+	 * Returns the root JSON value of the context.
+	 *
+	 * @return the root JSON value
+	 */
+	public JsonValue getRoot() {
+	    return root;
+	}
+
+	/**
+	 * Checks if this context is the head context.
+	 *
+	 * @return true if this context is the head, false otherwise
+	 */
+	public boolean isHead() {
+	    return head;
+	}
+
+	/**
+	 * Checks if this context is the tail context.
+	 *
+	 * @return true if this context is the tail, false otherwise
+	 */
+	public boolean isTail() {
+	    return !head;
+	}
+
+	/**
+	 * Returns the index JSON value of the context.
+	 *
+	 * @return the index JSON value
+	 */
+	public JsonValue getI() {
+	    return index;
+	}
+
+	/**
+	 * Returns the JSON object representing the context.
+	 *
+	 * @return the JSON object
+	 */
+	public JsonObject get$() {
+	    return context;
+	}
+
+	/**
+	 * Returns the last result JSON value of the context.
+	 *
+	 * @return the last result JSON value
+	 */
+	public JsonValue get_() {
+	    return lastResult;
+	}
+
+	/**
+	 * Returns a string representation of the context.
+	 *
+	 * @return a string representation of the context
+	 */
+	@Override
+	public String toString() {
+	    return "EL" + (expression == null ? ""
+	        : " expression \"" + expression + "\" at " + position + ": \""
+	        + expression.substring(Math.max(0, position - 5), position) + "["
+	        + (position >= 0 && position < expression.length() ? expression.charAt(position) : "") + "]"
+	        + expression.substring(Math.min(expression.length(), position + 1),
+	            Math.min(expression.length(), position + 6))
+	        + "\"" + (insertPoint >= 0 ? " insert at " + insertPoint : "")
+	        + (template && templatePath != null
+	            ? "\n   in " + templatePath
+	            + (templateBuffer == null ? "" : " = \"" + templateBuffer + "\"")
+	            : "")
+	        + (parent == null ? "" : "\nParent " + parent));
+	}
+
+}
---
+++ el/src/test/java/edu/iu/util/el/ElTest.java
@@ -0,0 +1,417 @@
+package edu.iu.util.el;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+import edu.iu.client.IuJsonAdapter;
+import jakarta.json.Json;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonValue;
+
+@SuppressWarnings("javadoc")
+public class ElTest {
+
+//	private static final Logger LOG = Logger.getLogger(ElTest.class.getName());
+
+	@Test
+	public void testEmptyExpression() {
+		assertNull(El.eval(null));
+		assertNull(El.eval(""));
+		assertEquals("Non-atmoic result", assertThrows(IllegalStateException.class, () -> El.eval(JsonValue.EMPTY_JSON_OBJECT, null)).getMessage());
+		assertEquals("Non-atmoic result", assertThrows(IllegalStateException.class, () -> El.eval(JsonValue.EMPTY_JSON_OBJECT, "")).getMessage());
+	}
+
+	@Test
+	public void testQuote() {
+		assertEquals("", IuJsonAdapter.of(String.class).fromJson(El.eval("'")));
+		assertEquals("Some stuff", IuJsonAdapter.of(String.class).fromJson(El.eval("'Some stuff")));
+	}
+
+	@Test
+	public void testComment() {
+		assertEquals("", IuJsonAdapter.of(String.class).fromJson(El.eval("* a comment")));
+		assertEquals("", IuJsonAdapter.of(String.class).fromJson(El.eval("'* a comment")));
+		assertEquals("* not a comment", IuJsonAdapter.of(String.class).fromJson(El.eval("'\\* not a comment")));
+		assertEquals("Some stuff", IuJsonAdapter.of(String.class).fromJson(El.eval("'Some stuff* with a comment")));
+		assertEquals("Some stuff* with a comment", IuJsonAdapter.of(String.class).fromJson(El.eval("'Some stuff\\* with a comment")));
+	}
+
+	@Test
+	public void testRaw() {
+		assertEquals("&lt;b&gt;Hello&lt;/b&gt; &quot;World&quot;", IuJsonAdapter.of(String.class).fromJson(El.eval("'<b>Hello</b> \"World\"")));
+		assertEquals("<b>Hello</b> \"World\"", IuJsonAdapter.of(String.class).fromJson(El.eval("@'<b>Hello</b> \"World\"")));
+	}
+
+	@Test
+	public void testResource() {
+		assertEquals("Hello World", IuJsonAdapter.of(String.class).fromJson(El.eval("<'el/hello.txt")));
+		assertEquals("Hello World", IuJsonAdapter.of(String.class).fromJson(El.eval("<'/el/hello.txt")));
+	}
+
+//	public static class BazBean {
+//
+//		private boolean success;
+//		private String message;
+//
+//		public boolean isSuccess() {
+//			return success;
+//		}
+//
+//		public void setSuccess(boolean success) {
+//			this.success = success;
+//		}
+//
+//		public String getMessage() {
+//			return message;
+//		}
+//
+//		public void setMessage(String message) {
+//			this.message = message;
+//		}
+//	}
+//
+//	public static class ExprTestBean {
+//
+//		private String foo;
+//		private boolean foot;
+//		private boolean foof;
+//		private boolean foob;
+//		private List<String> fooList;
+//		private Map<String, String> fooMap;
+//		private String fool;
+//		private float num;
+//		private Date now;
+//		private BazBean baz;
+//
+//		public String getFoo() {
+//			return foo;
+//		}
+//
+//		public void setFoo(String foo) {
+//			this.foo = foo;
+//		}
+//
+//		public boolean isFoot() {
+//			return foot;
+//		}
+//
+//		public void setFoot(boolean foot) {
+//			this.foot = foot;
+//		}
+//
+//		public boolean isFoof() {
+//			return foof;
+//		}
+//
+//		public void setFoof(boolean foof) {
+//			this.foof = foof;
+//		}
+//
+//		public boolean isFoob() {
+//			return foob;
+//		}
+//
+//		public void setFoob(boolean foob) {
+//			this.foob = foob;
+//		}
+//
+//		public List<String> getFooList() {
+//			return fooList;
+//		}
+//
+//		public void setFooList(List<String> fooList) {
+//			this.fooList = fooList;
+//		}
+//
+//		public Map<String, String> getFooMap() {
+//			return fooMap;
+//		}
+//
+//		public void setFooMap(Map<String, String> fooMap) {
+//			this.fooMap = fooMap;
+//		}
+//
+//		public String getFool() {
+//			return fool;
+//		}
+//
+//		public void setFool(String fool) {
+//			this.fool = fool;
+//		}
+//
+//		public float getNum() {
+//			return num;
+//		}
+//
+//		public void setNum(float num) {
+//			this.num = num;
+//		}
+//
+//		public Date getNow() {
+//			return now;
+//		}
+//
+//		public void setNow(Date now) {
+//			this.now = now;
+//		}
+//
+//		public BazBean getBaz() {
+//			return baz;
+//		}
+//
+//		public void setBaz(BazBean baz) {
+//			this.baz = baz;
+//		}
+//	}
+
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testBean() {
+//		ExprTestBean tb = new ExprTestBean();
+//
+//		tb.setFoo("bar");
+//		assertEquals("bar", El.eval(tb, "$.foo"));
+//
+//		tb.setFooList(Arrays.asList("foo", "bar", "baz"));
+//		assertEquals("bar", El.eval(tb, "$.fooList[1]"));
+//
+//		Map<String, String> fooMap = new LinkedHashMap<>();
+//		fooMap.put("foo", "bar");
+//		fooMap.put("bar", "bam");
+//		fooMap.put("bam", "foo");
+//		tb.setFooMap(fooMap);
+//		assertEquals("bar", El.eval(tb, "$.fooMap[\"foo\"]"));
+//		assertEquals("bam", El.eval(tb, "$.fooMap[$.foo]"));
+//		assertEquals("bar", El.eval(tb, "$.fooMap[$.fooMap[bam]]"));
+//	}
+
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testIntrospect() {
+//		ExprTestBean tb = new ExprTestBean();
+//		tb.setFoo("bar");
+//
+//		Map<String, Object> im = El.eval(tb, "@$&");
+//		LOG.info("im: " + im);
+//		assertEquals("bar", im.get("foo"));
+//		assertEquals(10, im.size());
+//		assertFalse(im.keySet().contains("class"));
+//		assertTrue(im.keySet().contains("fool"));
+//
+//		im = El.eval("@e&");
+//		LOG.info("im: " + im);
+//		assertEquals(2, im.size());
+//		assertFalse(im.keySet().contains("class"));
+//		assertTrue(im.keySet().contains("applicationUrl"));
+//		assertEquals("sisjee", im.get("applicationName"));
+//	}
+
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testConditional() {
+//		ExprTestBean tb = new ExprTestBean();
+//		tb.setFoof(true);
+//		assertEquals("bar", El.eval(tb, "$.foof?'bar!'foo"));
+//		assertEquals("bar", El.eval(tb, "$.foob?'baz!$.foof?'bar!'foo"));
+//	}
+//
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testTemplate() {
+//		ExprTestBean tb = new ExprTestBean();
+//		assertEquals("Here it is ", El.eval(tb, "<'el/testTemplate"));
+//
+//		BazBean baz = new BazBean();
+//		tb.setBaz(baz);
+//		assertEquals("Here it is a failure !", El.eval(tb, "<'el/testTemplate"));
+//
+//		baz.setMessage("to communicate");
+//		assertEquals("Here it is a failure to communicate!", El.eval(tb, "<'el/testTemplate"));
+//
+//		baz.setSuccess(true);
+//		baz.setMessage("- hooray");
+//		assertEquals("Here it is a success - hooray!", El.eval(tb, "<'el/testTemplate"));
+//	}
+//	
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testInlineTemplate() {
+//		String expr = "<`Here it is {$.baz?_<`a {$.success?'success!'`failure} {$.message}!`}`";
+//		ExprTestBean tb = new ExprTestBean();
+//		assertEquals("Here it is ", El.eval(tb, expr));
+//
+//		BazBean baz = new BazBean();
+//		tb.setBaz(baz);
+//		assertEquals("Here it is a `failure !", El.eval(tb, expr));
+//
+//		baz.setMessage("to communicate");
+//		assertEquals("Here it is a `failure to communicate!", El.eval(tb, expr));
+//
+//		baz.setSuccess(true);
+//		baz.setMessage("- hooray");
+//		assertEquals("Here it is a success - hooray!", El.eval(tb, expr));
+//	}
+//
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testFormat() {
+//		ExprTestBean tb = new ExprTestBean();
+//		tb.setNum(3.45f);
+//		assertEquals("3.450", El.eval(tb, "$.num##.000"));
+//		assertEquals("3.45", El.eval(tb, "$.num##.###"));
+//		tb.setNum(3.456f);
+//		assertEquals("3.456", El.eval(tb, "$.num##.###"));
+//		tb.setNum(3.456f);
+//		assertEquals("003.46", El.eval(tb, "$.num#000.00"));
+//
+//		Date now = new Date();
+//		tb.setNow(now);
+//		assertEquals(new SimpleDateFormat("MM/dd/yyyy HH:mm").format(now), El.eval(tb, "$.now#MM/dd/yyyy HH:mm"));
+//	}
+//
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testMatch() {
+//		ExprTestBean tb = new ExprTestBean();
+//
+//		tb.setFoo("bar");
+//		assertEquals(true, El.eval(tb, "$.foo='bar"));
+//
+//		tb.setNum(12.34f);
+//		assertEquals(true, El.eval(tb, "$.num='12.34"));
+//	}
+//
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testList() {
+//		ExprTestBean tb = new ExprTestBean();
+//
+//		tb.setFooList(Arrays.asList("foo", "bar", "baz"));
+//		assertEquals(tb.getFooList(), El.eval(tb, "@$.fooList"));
+//		assertEquals("[foo, bar, baz]", El.eval(tb, "$.fooList"));
+//		assertEquals("foo,bar,baz", El.eval(tb, "$.fooList<'el/-list"));
+//		assertEquals(" 0: foo\r\n 1: bar\r\n 2: baz", El.eval(tb, "$.fooList<'el/-hash"));
+//	}
+//
+//	@Ignore // TODO: REMOVE
+//	@Test
+//	public void testMap() {
+//		ExprTestBean tb = new ExprTestBean();
+//		Map<String, String> fooMap = new LinkedHashMap<>();
+//		fooMap.put("foo", "bar");
+//		fooMap.put("baz", "bif");
+//		fooMap.put("bim", "bam");
+//		tb.setFooMap(fooMap);
+//		assertEquals(" foo: bar\r\n baz: bif\r\n bim: bam", El.eval(tb, "$.fooMap<'el/-hash"));
+//	}
+//
+//	@Ignore // TODO: REMOVE or allow beans in El.eval
+//	@Test
+//	public void testTemplateExpr() {
+//		ExprTestBean tb = new ExprTestBean();
+//		tb.setFool("el/-list");
+//		tb.setFooList(Arrays.asList("foo", "bar", "baz"));
+//		assertEquals("foo,bar,baz", El.eval(tb, "$.fooList<p.$.fool"));
+//	}
+
+	@Test
+	public void testJson() {
+		JsonObjectBuilder b = Json.createObjectBuilder();
+		b.add("foo", "bar");
+		b.add("baz", "bif");
+		b.add("bim", "bam");
+		assertEquals("bar bif bam", IuJsonAdapter.of(String.class).fromJson(El.eval(b.build(), "<`{$.foo} {$.baz} {$.bim}`")));
+		assertEquals("inline template doesn't end with '`'", assertThrows(IllegalArgumentException.class, () -> El.eval(b.build(), "<`{$.foo} {$.baz} {$.bim}")).getMessage());
+		assertEquals("inline template doesn't end with '`'", assertThrows(IllegalArgumentException.class, () -> El.eval(b.build(), "<`")).getMessage());
+	}
+
+	@Test
+	public void testIfConditional() {
+		JsonObjectBuilder b = Json.createObjectBuilder();
+		b.add("foo", true);
+		b.add("baz", false);
+		b.add("bim", "bam");
+		b.add("bum", JsonValue.NULL);
+		final var context = b.build();
+		assertEquals("foo is true", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.foo?'foo is true")));
+		assertEquals("foo is true", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.foo?'foo is true!'foo is false")));
+		// TODO: if we decide JsonValue.NULL is false, this should move to testUnlessConditional
+		assertEquals("bum is true", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bum?'bum is true!'bum is false")));
+		assertEquals("bim exists", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bim?'bim exists!'bim does not exist")));
+		assertEquals(JsonValue.FALSE, El.eval(context, "$.baz?'baz is true"));
+		assertEquals(JsonValue.TRUE, El.eval(context, "$.foo!'foo is false"));
+		assertNull(El.eval("$?'no context"));
+	}
+
+	@Test
+	public void testUnlessConditional() {
+		JsonObjectBuilder b = Json.createObjectBuilder();
+		b.add("foo", true);
+		b.add("baz", false);
+		b.add("bim", "bam");
+		b.add("bum", JsonValue.NULL);
+		final var context = b.build();
+		assertEquals("baz is false", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.baz?'baz is true!'baz is false")));
+		assertEquals("baz is false", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.baz!'baz is false")));
+		assertEquals("bif is null", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bif?'bif is not null!'bif is null")));
+		assertEquals("bif is null", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bif!'bif is null")));
+	}
+
+	@Test
+	public void testMatchConditional() {
+		JsonObjectBuilder b = Json.createObjectBuilder();
+		b.add("foo", true);
+		b.add("baz", false);
+		b.add("bim", "bam");
+		final var context = b.build();
+		assertEquals(JsonValue.TRUE, El.eval(context, "$.bim='bam"));
+		assertEquals(JsonValue.FALSE, El.eval(context, "$.bim='baz"));
+	}
+
+	@Test
+	public void testFormat() {
+		JsonObjectBuilder b = Json.createObjectBuilder();
+		b.add("int", 1);
+		b.add("string_int", "0123");
+		b.add("bigger_int", 1234567890);
+		b.add("pi", 3.14159);
+		b.add("money", "3.50");
+		b.add("bigger_money", "1234567890.97");
+		final var context = b.build();
+		// numbers
+		assertEquals("1", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.int#0")));
+		assertEquals("1", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.int##")));
+		assertEquals("01", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.int#00")));
+
+		assertEquals("0123", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.string_int#00")));
+
+		assertEquals("1234567890", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bigger_int#0000000000")));
+		assertEquals("1,234,567,890", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bigger_int#0,000,000,000")));
+		assertEquals("1234567890", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bigger_int##")));
+		assertEquals("0,12,34,56,78,90", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bigger_int#0,00,00,00,00,00")));
+		assertEquals("1,234,567,890", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.bigger_int####,###,###,###")));
+
+		assertEquals("3", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi#0")));
+		assertEquals("3", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi##")));
+		assertEquals("03", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi#00")));
+		assertEquals("3.14", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi#0.00")));
+		assertEquals("3.14", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi####.0#")));
+		assertEquals("03.14", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi#00.00")));
+		assertEquals("3.14159", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi##.#####")));
+		assertEquals("3.141590", IuJsonAdapter.of(String.class).fromJson(El.eval(context, "$.pi##.000000")));
+
+		// dates
+		
+		// ignored
+	}
+	
+	@Test
+	public void testExecutionContext() {
+		
+	}
+}
