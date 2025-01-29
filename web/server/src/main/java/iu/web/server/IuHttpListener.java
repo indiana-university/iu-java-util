ee3504cc4bf454185123b8a4efd7f035b3430ed7
anderjak <anderjak@iu.edu>
2025-01-29 22:08:35 +0000

    STARCH-934 WIP add serving from zip file to IuHttpListener with tests

--- web/server/src/main/java/iu/web/server/IuHttpListener.java
+++ web/server/src/main/java/iu/web/server/IuHttpListener.java
@@ -1,16 +1,33 @@
 package iu.web.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.UnaryOperator;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 import java.util.logging.Logger;
-
-import org.apache.commons.text.StringEscapeUtils;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpHandlers;
 import com.sun.net.httpserver.HttpServer;
 
-import edu.iu.IuText;
+import edu.iu.IuException;
+import edu.iu.IuStream;
+import edu.iu.UnsafeFunction;
 
 /**
  * {@link HttpServer} configuration wrapper.
@@ -18,6 +35,37 @@ import edu.iu.IuText;
 public final class IuHttpListener implements AutoCloseable {
 
 	private static final Logger LOG = Logger.getLogger(IuHttpListener.class.getName());
+
+	// Default for unknown content types, as per RFC 2046
+//	private static final String DEFAULT_CONTENT_TYPE = "application/octet-stream";
+
+	private static final Map<String, String> MIME_TABLE = new ConcurrentHashMap<>();
+	private static final Map<String, Path> FILE_MAP = new ConcurrentHashMap<>();
+//	private static final String FILES_ZIP = "/ess-frontend-template-bundle.zip";
+
+	static {
+		String[] mimeTypes = { "html", "text/html", "js", "application/javascript", "js.map", "application/json", "css",
+				"text/css", "png", "image/png", "jpg", "image/jpeg", "jpeg", "image/jpeg", "gif", "image/gif", "svg",
+				"image/svg+xml" };
+		for (int i = 0; i < mimeTypes.length; i += 2) {
+			MIME_TABLE.put(mimeTypes[i], mimeTypes[i + 1]);
+		}
+		
+		// Get the list of zip files in the directory /opt/starch/resources
+		// TODO: This should be configurable
+        Path dir = Paths.get("/opt/starch/resources");
+
+        DirectoryStream<Path> stream = IuException.unchecked(() -> Files.newDirectoryStream(dir, "*.zip"));
+        for (Path entry : stream) {
+            URL fileUrl = IuException.unchecked(() -> entry.toUri().toURL());
+            FILE_MAP.putAll(bootstrapStaticFileServer(fileUrl));
+        }
+        IuException.unchecked(() -> stream.close());
+	}
+
+	private static final UnaryOperator<String> GET_MIME_TYPE = extension -> {
+		return MIME_TABLE.getOrDefault(extension, URLConnection.getFileNameMap().getContentTypeFor(extension));
+	};
 
 	private final int stopDelay;
 	private volatile HttpServer server;
@@ -33,24 +81,146 @@ public final class IuHttpListener implements AutoCloseable {
 	 */
 	public static IuHttpListener create(InetSocketAddress localAddress, int backlog, int stopDelay) throws IOException {
 		final var server = HttpServer.create(localAddress, backlog);
-		server.createContext("/", new HttpHandler() {
-			@Override
-			public void handle(HttpExchange exchange) throws IOException {
-				exchange.getResponseHeaders().add("content-type", "text/html");
-				exchange.sendResponseHeaders(200, 0);
-				try (final var body = exchange.getResponseBody()) {
-					body.write(IuText.utf8("<html><body><p>TODO: implement "
-							+ StringEscapeUtils.escapeHtml4(exchange.getRequestURI().toString())
-							+ "</p></body></html>"));
-				}
+		server.createContext("/", HttpHandlers.handleOrElse((req) -> {
+			if (!req.getRequestMethod().equals("GET")) {
+				LOG.warning("files context method not allowed: " + req.getRequestMethod());
+				return false;
 			}
-		});
+			String reqPath = req.getRequestURI().getPath();
+			LOG.info("path: " + reqPath);
+			return true;
+		},
+				new HttpHandler() {
+					@Override
+					public void handle(HttpExchange exchange) throws IOException {
+						LOG.info("file handler handling request URI: " + exchange.getRequestURI().toString());
+						// get the content type from the file extension in the request URI
+						String reqPath = exchange.getRequestURI().getPath();
+						String assetPath = reqPath.substring(1); // remove leading /
+						// if assetPath is empty or no filename is given, assume the request is for an
+						// index.html file
+						if (assetPath.isEmpty() //
+								|| assetPath.endsWith("/")) {
+							assetPath += "index.html";
+						}
+						// special handling for .js.map files
+						int extIndex = 0;
+						if (assetPath.endsWith(".js.map")) {
+							extIndex = assetPath.length() - 7;
+						} else {
+							extIndex = assetPath.lastIndexOf('.');
+						}
+						String ext = assetPath.substring(extIndex + 1);
+						String contentType = GET_MIME_TYPE.apply(ext);
+						exchange.getResponseHeaders().add("content-type", contentType);
+
+						Path file = FILE_MAP.get(assetPath);
+						if (file == null //
+								|| !Files.exists(file)) {
+							LOG.warning("file not found: " + reqPath);
+							exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+							return;
+						}
+						// get the file from the map of jar file entries
+						final var contents = Files.readAllBytes(file);
+						exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+						try (final var body = exchange.getResponseBody()) {
+							body.write(contents);
+						}
+					}
+				}, (exchange) -> {
+					// TODO: fallback handler
+					LOG.info("files context fallback. request URI: " + exchange.getRequestURI().toString());
+				}));
 		server.setExecutor(null); // creates a default executor
 		server.start();
 
 		final var listener = new IuHttpListener(server, stopDelay);
 		LOG.fine(() -> "started " + listener);
 		return listener;
+	}
+
+	/**
+	 * @param bundleResource
+	 * @return a map of file paths to temporary files
+	 */
+	public static Map<String, Path> bootstrapStaticFileServer(URL bundleResource) {
+		Map<String, Path> fileMap = new ConcurrentHashMap<>();
+		with(bundleResource, o -> {
+			try (final var in = Files.newInputStream((Path) o); //
+					final var bundleZip = new ZipInputStream(in)) {
+				
+				ZipEntry entry;
+				while ((entry = bundleZip.getNextEntry()) != null) {
+					final var name = entry.getName();
+					if (!entry.isDirectory()) {
+						String fileNameWithExt = name.substring(name.lastIndexOf('/') + 1);
+						// special handling for .js.map files
+						int extIndex = 0;
+						if (fileNameWithExt.endsWith(".js.map")) {
+							extIndex = fileNameWithExt.length() - 7;
+						} else {
+							extIndex = fileNameWithExt.lastIndexOf('.');
+						}
+						
+						String fileName = fileNameWithExt.substring(0, extIndex) + "-";
+						String ext = fileNameWithExt.substring(extIndex);
+						// Simplistic way to create a temporary file to hold the entry, rather than working around the iu-type....jar naming
+						Path temp = Files.createTempFile(fileName, ext);
+						try (final var out = Files.newOutputStream(temp)) {
+							IuStream.copy(bundleZip, out);
+						}
+						fileMap.put(name, temp);
+					}
+				}
+				bundleZip.closeEntry();
+			}
+			return null;
+		});
+		return fileMap;
+	}
+
+//	public static Iterable<Path> readBundle(URL bundleResource) {
+//		final Deque<Path> libs = new ArrayDeque<>();
+//		with(bundleResource, o -> {
+//			try (final var in = (o instanceof Path) //
+//					? Files.newInputStream((Path) o) //
+//					: (InputStream) o; //
+//					final var bundleJar = new JarInputStream(in)) {
+//				JarEntry entry;
+//				while ((entry = bundleJar.getNextJarEntry()) != null) {
+//					final var name = entry.getName();
+//					if (name.endsWith(".jar")) {
+//						final var lib = name.startsWith("lib/");
+//						final var bundledLib = TemporaryFile.init(path -> {
+//							try (final var out = Files.newOutputStream(path)) {
+//								IuStream.copy(bundleJar, out);
+//							}
+//							return path;
+//						});
+//						if (lib)
+//							libs.offer(bundledLib);
+//						else
+//							libs.offerFirst(bundledLib);
+//					}
+//				}
+//				bundleJar.closeEntry();
+//			}
+//			return null;
+//		});
+//		return libs;
+//	}
+
+	private static <T> T with(URL url, UnsafeFunction<Object, T> then) {
+		return IuException.unchecked(() -> {
+			final var uri = url.toURI();
+			final var scheme = uri.getScheme();
+
+			if ("file".equals(scheme))
+				return then.apply(Path.of(uri).toRealPath());
+
+			throw new IllegalArgumentException();
+		});
 	}
 
 	private IuHttpListener(HttpServer server, int stopDelay) {
--- web/server/src/test/java/iu/web/server/IuHttpListenerTest.java
+++ web/server/src/test/java/iu/web/server/IuHttpListenerTest.java
@@ -1,8 +1,11 @@
 package iu.web.server;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
@@ -12,6 +15,12 @@ import static org.mockito.Mockito.when;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
 
@@ -24,31 +33,38 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import edu.iu.IdGenerator;
+import edu.iu.IuException;
 import edu.iu.IuText;
+import edu.iu.test.IuTest;
 import edu.iu.test.IuTestLogger;
 
 @SuppressWarnings("javadoc")
 public class IuHttpListenerTest {
 
 	@Test
-	public void testStartStop() {
+	public void testStartStopGetIndex() {
 		final var server = mock(HttpServer.class);
 		final var address = mock(InetSocketAddress.class);
 		final int backlog = ThreadLocalRandom.current().nextInt();
 		final int stopDelay = ThreadLocalRandom.current().nextInt();
-		try (final var mockHttpServer = mockStatic(HttpServer.class)) {
+		try (final var mockHttpServer = mockStatic(HttpServer.class);final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
 			mockHttpServer.when(() -> HttpServer.create(address, backlog)).thenReturn(server);
 
+			final var requestUri = "http://localhost:8080/";
 			IuTestLogger.expect(IuHttpListener.class.getName(), Level.FINE,
 					"started IuHttpListener [stopDelay=" + stopDelay + ", server=" + server + ']');
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /");
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "file handler handling request URI: " + requestUri);
 
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
 			final var listener = assertDoesNotThrow(() -> IuHttpListener.create(address, backlog, stopDelay));
 			verify(server).createContext(eq("/"), argThat((HttpHandler a) -> {
 				final var exchange = mock(HttpExchange.class);
 				when(exchange.getRequestMethod()).thenReturn("GET");
 
-				final var requestUri = URI.create(IdGenerator.generateId());
-				when(exchange.getRequestURI()).thenReturn(requestUri);
+				when(exchange.getRequestURI()).thenReturn(URI.create(requestUri));
 
 				final var headers = mock(Headers.class);
 				when(exchange.getResponseHeaders()).thenReturn(headers);
@@ -74,6 +90,197 @@ public class IuHttpListenerTest {
 
 			assertDoesNotThrow(listener::close);
 			verifyNoMoreInteractions(server);
+		}
+	}
+
+	@Test
+	public void testPost() {
+		final var server = mock(HttpServer.class);
+		final var address = mock(InetSocketAddress.class);
+		final int backlog = ThreadLocalRandom.current().nextInt();
+		final int stopDelay = ThreadLocalRandom.current().nextInt();
+		try (final var mockHttpServer = mockStatic(HttpServer.class);final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
+			mockHttpServer.when(() -> HttpServer.create(address, backlog)).thenReturn(server);
+
+			final var requestUri = "http://localhost:8080/";
+			IuTestLogger.expect(IuHttpListener.class.getName(), Level.FINE,
+					"started IuHttpListener [stopDelay=" + stopDelay + ", server=" + server + ']');
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /");
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.WARNING, "files context method not allowed: POST");
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "files context fallback. request URI: " + requestUri);
+
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
+			final var listener = assertDoesNotThrow(() -> IuHttpListener.create(address, backlog, stopDelay));
+			verify(server).createContext(eq("/"), argThat((HttpHandler a) -> {
+				final var exchange = mock(HttpExchange.class);
+				when(exchange.getRequestMethod()).thenReturn("POST");
+
+				when(exchange.getRequestURI()).thenReturn(URI.create(requestUri));
+
+				final var headers = mock(Headers.class);
+				when(exchange.getResponseHeaders()).thenReturn(headers);
+				final var body = mock(OutputStream.class);
+				when(exchange.getResponseBody()).thenReturn(body);
+
+				assertDoesNotThrow(() -> a.handle(exchange));
+
+				return true;
+			}));
+		}
+	}
+
+	@Test
+	public void testNotFound() {
+		final var server = mock(HttpServer.class);
+		final var address = mock(InetSocketAddress.class);
+		final int backlog = ThreadLocalRandom.current().nextInt();
+		final int stopDelay = ThreadLocalRandom.current().nextInt();
+		try (final var mockHttpServer = mockStatic(HttpServer.class);final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
+			mockHttpServer.when(() -> HttpServer.create(address, backlog)).thenReturn(server);
+
+			final var baseAddress = "http://localhost:8080/";
+			final var reqPath = "notExists";
+			final var requestUri = baseAddress + reqPath;
+			IuTestLogger.expect(IuHttpListener.class.getName(), Level.FINE,
+					"started IuHttpListener [stopDelay=" + stopDelay + ", server=" + server + ']');
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /" + reqPath);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /");
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "file handler handling request URI: " + requestUri);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "file handler handling request URI: " + baseAddress);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.WARNING, "file not found: /" + reqPath);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.WARNING, "file not found: /");
+			
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
+			final var listener = assertDoesNotThrow(() -> IuHttpListener.create(address, backlog, stopDelay));
+			verify(server).createContext(eq("/"), argThat((HttpHandler a) -> {
+				final var exchange = mock(HttpExchange.class);
+				when(exchange.getRequestMethod()).thenReturn("GET");
+
+				when(exchange.getRequestURI()).thenReturn(URI.create(requestUri));
+
+				final var headers = mock(Headers.class);
+				when(exchange.getResponseHeaders()).thenReturn(headers);
+				final var body = mock(OutputStream.class);
+				when(exchange.getResponseBody()).thenReturn(body);
+
+				assertDoesNotThrow(() -> a.handle(exchange));
+				
+				try (final var mockFiles = mockStatic(Files.class)) {
+					mockFiles.when(() -> Files.isRegularFile(any())).thenReturn(false);
+					final var deletedTempExchange = mock(HttpExchange.class);
+					when(deletedTempExchange.getRequestMethod()).thenReturn("GET");
+					when(deletedTempExchange.getRequestURI()).thenReturn(URI.create(baseAddress));
+
+					when(deletedTempExchange.getResponseHeaders()).thenReturn(headers);
+					when(deletedTempExchange.getResponseBody()).thenReturn(body);
+
+					assertDoesNotThrow(() -> a.handle(deletedTempExchange));
+				}
+
+				return true;
+			}));
+		}
+	}
+
+	@Test
+	public void testGetJsMap() {
+		final var server = mock(HttpServer.class);
+		final var address = mock(InetSocketAddress.class);
+		final int backlog = ThreadLocalRandom.current().nextInt();
+		final int stopDelay = ThreadLocalRandom.current().nextInt();
+		try (final var mockHttpServer = mockStatic(HttpServer.class);final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
+			mockHttpServer.when(() -> HttpServer.create(address, backlog)).thenReturn(server);
+
+			final var reqPath = "assets/main.js.map";
+			final var requestUri = "http://localhost:8080/" + reqPath;
+			IuTestLogger.expect(IuHttpListener.class.getName(), Level.FINE,
+					"started IuHttpListener [stopDelay=" + stopDelay + ", server=" + server + ']');
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /" + reqPath);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "file handler handling request URI: " + requestUri);
+
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
+			final var listener = assertDoesNotThrow(() -> IuHttpListener.create(address, backlog, stopDelay));
+			verify(server).createContext(eq("/"), argThat((HttpHandler a) -> {
+				final var exchange = mock(HttpExchange.class);
+				when(exchange.getRequestMethod()).thenReturn("GET");
+
+				when(exchange.getRequestURI()).thenReturn(URI.create(requestUri));
+
+				final var headers = mock(Headers.class);
+				when(exchange.getResponseHeaders()).thenReturn(headers);
+				final var body = mock(OutputStream.class);
+				when(exchange.getResponseBody()).thenReturn(body);
+
+				assertDoesNotThrow(() -> a.handle(exchange));
+				verify(headers).add("content-type", "application/json");
+				assertDoesNotThrow(() -> verify(exchange).sendResponseHeaders(200, 0));
+				// TODO: verify body?
+//				assertDoesNotThrow(() -> verify(body).write(IuText.utf8("<html><body><p>TODO: implement "
+//						+ StringEscapeUtils.escapeHtml4(exchange.getRequestURI().toString()) + "</p></body></html>")));
+
+				return true;
+			}));
+		}
+	}
+
+	@Test
+	public void testGetDeepIndex() {
+		final var server = mock(HttpServer.class);
+		final var address = mock(InetSocketAddress.class);
+		final int backlog = ThreadLocalRandom.current().nextInt();
+		final int stopDelay = ThreadLocalRandom.current().nextInt();
+		try (final var mockHttpServer = mockStatic(HttpServer.class);final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
+			mockHttpServer.when(() -> HttpServer.create(address, backlog)).thenReturn(server);
+
+			final var reqPath = "src/other/";
+			final var requestUri = "http://localhost:8080/" + reqPath;
+			IuTestLogger.expect(IuHttpListener.class.getName(), Level.FINE,
+					"started IuHttpListener [stopDelay=" + stopDelay + ", server=" + server + ']');
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "path: /" + reqPath);
+			IuTestLogger.allow(IuHttpListener.class.getName(), Level.INFO, "file handler handling request URI: " + requestUri);
+
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
+			final var listener = assertDoesNotThrow(() -> IuHttpListener.create(address, backlog, stopDelay));
+			verify(server).createContext(eq("/"), argThat((HttpHandler a) -> {
+				final var exchange = mock(HttpExchange.class);
+				when(exchange.getRequestMethod()).thenReturn("GET");
+
+				when(exchange.getRequestURI()).thenReturn(URI.create(requestUri));
+
+				final var headers = mock(Headers.class);
+				when(exchange.getResponseHeaders()).thenReturn(headers);
+				final var body = mock(OutputStream.class);
+				when(exchange.getResponseBody()).thenReturn(body);
+
+				assertDoesNotThrow(() -> a.handle(exchange));
+				verify(headers).add("content-type", "text/html");
+				assertDoesNotThrow(() -> verify(exchange).sendResponseHeaders(200, 0));
+				assertDoesNotThrow(() -> verify(body).write(IuText.utf8("<html><body><p>TODO: implement "
+						+ StringEscapeUtils.escapeHtml4(exchange.getRequestURI().toString()) + "</p></body></html>")));
+
+				return true;
+			}));
+
+		}
+	}
+	
+	@Test
+	public void testBootstrapStaticFileServerWithNonFileURL() {
+		try (final var paths = mockStatic(Paths.class, CALLS_REAL_METHODS)) {
+			final var archivePathProperty = IuTest.getProperty("teststatic.archive");
+			final var archivePath = Path.of(archivePathProperty);
+			paths.when(() -> Paths.get("/opt/starch/resources")).thenReturn(archivePath);
+
+			final var url = IuException.unchecked(() -> URI.create("http://localhost:8080/").toURL());
+			assertThrows(IllegalArgumentException.class, () -> IuHttpListener.bootstrapStaticFileServer(url));
 		}
 	}
 
