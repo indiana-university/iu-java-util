cd34cc3db43f532ea1210bba58413851d07af4c5
Mark Fyffe <mwfyffe@iu.edu>
2025-03-18 06:07:14 -0400

    STARCH-974 move subscription received event before tryAdvance

--- base/src/test/java/edu/iu/AsynchronousSubjectTest.java
+++ base/src/test/java/edu/iu/AsynchronousSubjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2025 Indiana University
+ * Copyright © 2024 Indiana University
  * All rights reserved.
  *
  * BSD 3-Clause License
@@ -323,8 +323,7 @@ public class AsynchronousSubjectTest {
 			final var subscriber = subject.subscribe();
 			final var subsplit = subscriber.stream().spliterator();
 			subject.accept("b");
-			assertNull(subsplit.trySplit()); // not exhausted
-			assertTrue(subsplit.tryAdvance(a -> assertEquals("a", a)));
+			assertTrue(subsplit.trySplit().tryAdvance(a -> assertEquals("a", a)));
 			assertTrue(subsplit.tryAdvance(a -> assertEquals("b", a)));
 			assertNull(subsplit.trySplit()); // bootstrap pipe
 			assertNull(subsplit.trySplit()); // reuse pipe
