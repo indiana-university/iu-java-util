1366e2cdfd887438e04658026f4e32645adfb5f6
eshrastg <ess-dev-l@iu.edu>
2025-08-21 19:26:53 +0000

    clean feature/STARCH-934/logging_import

--- docs/feature/STARCH-934/logging_import/auth/config/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"iu.util.auth.config","l":"edu.iu.auth.config"},{"m":"iu.util.auth.config","l":"iu.auth.config"}];updateSearchResults();
