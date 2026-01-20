5df217ae04f14e5c2b49639e73d571eb0c92a610
mramawat <mramawat@iu.edu>
2026-01-20 15:28:45 -0500

    fix build error and code cleanup

--- auth/saml/src/main/java/iu/auth/saml/SamlBuilder.java
+++ auth/saml/src/main/java/iu/auth/saml/SamlBuilder.java
@@ -35,10 +35,10 @@ import java.net.URI;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.opensaml.core.config.ConfigurationService;
 import org.opensaml.core.criterion.EntityIdCriterion;
@@ -172,7 +172,7 @@ final class SamlBuilder {
 	private Instant lastMetadataUpdate;
 
 	/** credential resolver for validating the SAMLResponse signature */
-	ConcurrentHashMap<String, CredentialResolver> credentialResolverMap = new ConcurrentHashMap<>();
+	HashMap<String, CredentialResolver> credentialResolverMap = new HashMap<>();
 
 	/**
 	 * Constructor
@@ -226,33 +226,6 @@ final class SamlBuilder {
 			if (this.singleSignOnLocation == null)
 			this.singleSignOnLocation = singleSignOnLocation;
 		}
-		
-		/*final var entity = Objects.requireNonNull(
-				IuException.unchecked(() -> resolver.resolveSingle(new CriteriaSet(new EntityIdCriterion(entityId)))),
-				"Entity " + entityId + " not found in SAML metadata");*/
-
-		//final var idp = Objects.requireNonNull(entity.getIDPSSODescriptor("urn:oasis:names:tc:SAML:2.0:protocol"),
-			//	"Missing SAML 2.0 IDP descriptor for " + entityId);
-
-		/*URI singleSignOnLocation = null;
-		for (final var sso : idp.getSingleSignOnServices())
-			if ("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect".equals(sso.getBinding())) {
-				singleSignOnLocation = URI.create(sso.getLocation());
-				break;
-			}
-
-		if (singleSignOnLocation == null)
-			throw new IllegalStateException("Missing SAML 2.0 Redirect Binding in IDP descriptor for " + entityId);
-
-		this.singleSignOnLocation = singleSignOnLocation;
-
-		List<Credential> certs = new ArrayList<>();
-		for (final var keyDescriptor : idp.getKeyDescriptors())
-			for (final var x509data : keyDescriptor.getKeyInfo().getX509Datas())
-				for (final var x509cert : x509data.getX509Certificates())
-					certs.add(new BasicX509Credential(PemEncoded.parse(x509cert.getValue()).next().asCertificate()));
-
-		credentialResolver = new StaticCredentialResolver(certs);*/
 	}
 
 	/**
