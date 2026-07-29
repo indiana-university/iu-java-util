35e5e50eb8c133732b553ac50af1bb281a5e9e51
eshrastg <ess-dev-l@iu.edu>
2026-07-29 17:14:29 -0400

    publish mwfyffeiu feature/STARCH-1075/dao_migration

--- docs/feature/STARCH-1075/dao_migration/jdbc/pool/apidocs/package-search-index.js
+++ docs/feature/STARCH-1075/dao_migration/jdbc/pool/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"iu.util.jdbc.pool","l":"edu.iu.jdbc.pool"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"m":"iu.util.jdbc.pool","l":"edu.iu.jdbc.pool"},{"m":"iu.util.jdbc.pool","l":"iu.jdbc.pool.config"}];updateSearchResults();
